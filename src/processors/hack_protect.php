c6f9e604cb3f1307e445381b37e1af69fc4a9f9c
Paul Goodchild <githubs@paulgoodchild.net>
2015-07-07 10:28:12 +0100

    fix for unsupported syntax for lower versions of php

--- src/processors/hack_protect.php
+++ src/processors/hack_protect.php
@@ -20,8 +20,15 @@ if ( !class_exists( 'ICWP_WPSF_Processor_HackProtect_V1', false ) ):
 			// not probably necessary any longer since it's patched in the Core
 			add_filter( 'pre_comment_content', array( $this, 'secXss64kb' ), 0, 1 );
 
+			$this->runPluginVulnerabilities();
+		}
+
+		/**
+		 */
+		protected function runPluginVulnerabilities() {
 			require_once( dirname(__FILE__).ICWP_DS.'hackprotect_pluginvulnerabilities.php' );
-			( new ICWP_WPSF_Processor_HackProtect_PluginVulnerabilities( $this->getFeatureOptions() ) )->run();
+			$oPv = new ICWP_WPSF_Processor_HackProtect_PluginVulnerabilities( $this->getFeatureOptions() );
+			$oPv->run();
 		}
 
 		/**
