72c48711612c6c7493966e344326bcdff0549175
Paul G <paul@icontrolwp.com>
2015-08-27 15:36:23 +0100

    ip list tables print different contents based on type

--- templates/php/snippets/ip_list_table.php
+++ templates/php/snippets/ip_list_table.php
@@ -3,48 +3,59 @@ $sTableId = uniqid('IpTable').rand(0,1000);
 ?>
 
 <table class="table table-bordered" id="<?php echo $sTableId; ?>">
-<?php if ( empty( $list_data ) ) : ?>
-	<tr>
-		<td colspan="4">
-			There are no IPs on this list.
-			<a href="javascript:refresh_list( '<?php echo $list_id; ?>', jQuery('#<?php echo $sTableId; ?>').parent() );">Refresh</a>
-		</td>
-	</tr>
-<?php else: ?>
-
-	<tr>
-		<th>IP Address</th>
-		<th>Transgressions</th>
-		<th>Last Access</th>
-		<th>Actions</th>
-	</tr>
-	<?php foreach( $list_data as $aIpData ) : ?>
+	<?php if ( empty( $list_data ) ) : ?>
 		<tr>
-			<td><?php echo $aIpData['ip']; ?></td>
-			<td><?php echo $aIpData['transgressions']; ?></td>
-			<td><?php echo $aIpData['last_access_at']; ?></td>
-			<td><a href="javascript:remove_ip( '<?php echo $aIpData['ip']; ?>', '<?php echo $aIpData['list']; ?>' );">Remove</a></td>
+			<td colspan="4">
+				There are no IPs on this list.
+				<a href="javascript:refresh_list( '<?php echo $list_id; ?>', jQuery('#<?php echo $sTableId; ?>').parent() );">Refresh</a>
+			</td>
 		</tr>
-	<?php endforeach; ?>
-	<tr>
-		<td colspan="4">
-			<a href="javascript:refresh_list( '<?php echo $list_id; ?>', jQuery('#<?php echo $sTableId; ?>').parent() );">Refresh</a>
-		</td>
-	</tr>
+	<?php else: ?>
 
-<?php endif; ?>
+		<tr>
+			<th>IP Address</th>
 
-<?php if ( $list_id == 'MW' ) : ?>
-	<tr>
-		<td colspan="4">
-			<div class="input-append">
-				<input class="span4" name="new_ip" placeholder="Add IP Address" id="AddIpAddress" type="text">
-				<button class="btn" type="button" id="AddIpButton">
-					Click To Add!</button>
-			</div>
-		</td>
-	</tr>
-<?php endif; ?>
+			<?php if ( $list_id == 'MW' ) : ?>
+				<th>Label</th>
+			<?php else: ?>
+				<th>Transgressions</th>
+			<?php endif; ?>
+
+			<th>Last Access</th>
+			<th>Actions</th>
+		</tr>
+
+		<?php foreach( $list_data as $aIpData ) : ?>
+			<tr>
+				<td><?php echo $aIpData['ip']; ?></td>
+				<?php if ( $list_id == 'MW' ) : ?>
+					<td><?php echo $aIpData['label']; ?></td>
+				<?php else: ?>
+					<td><?php echo $aIpData['transgressions']; ?></td>
+				<?php endif; ?>
+				<td><?php echo $aIpData['last_access_at']; ?></td>
+				<td><a href="javascript:remove_ip( '<?php echo $aIpData['ip']; ?>', '<?php echo $aIpData['list']; ?>' );">Remove</a></td>
+			</tr>
+		<?php endforeach; ?>
+		<tr>
+			<td colspan="4">
+				<a href="javascript:refresh_list( '<?php echo $list_id; ?>', jQuery('#<?php echo $sTableId; ?>').parent() );">Refresh</a>
+			</td>
+		</tr>
+
+	<?php endif; ?>
+
+	<?php if ( $list_id == 'MW' ) : ?>
+		<tr>
+			<td colspan="4">
+				<div class="input-append">
+					<input class="span4" name="new_ip" placeholder="Add IP Address" id="AddIpAddress" type="text">
+					<button class="btn" type="button" id="AddIpButton">Click To Add!</button>
+				</div>
+			</td>
+		</tr>
+	<?php endif; ?>
+
 </table>
 
 <script type="text/javascript" >
@@ -53,6 +64,11 @@ $sTableId = uniqid('IpTable').rand(0,1000);
 
 	jQuery( document ).ready(function() {
 		jQuery('#AddIpButton', $oTable).click( add_ip_to_whitelist );
+		jQuery('#AddIpAddress', $oTable).keypress(function(e) {
+			if(e.which == 13) {
+				add_ip_to_whitelist();
+			}
+		});
 	});
 
 	function remove_ip( $sIp, $sList ) {
