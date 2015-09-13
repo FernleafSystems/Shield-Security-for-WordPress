dc8d9a206cd99f6464c857a854782507bad505cc
Paul G <paul@icontrolwp.com>
2015-09-13 17:02:24 +0100

    first draft of rate plugin notice

--- templates/php/notices/rate-plugin.php
+++ templates/php/notices/rate-plugin.php
@@ -1,1 +1,38 @@
-	rate plugin
+<div>
+	<div class="notice-icon">
+		<span class="dashicons dashicons-heart"></span>&nbsp;
+	</div>
+
+	<div class="notice-content">
+		<h3>Would you like show us your support?  We need only your words.</h3>
+		<p>The authors of the Simple Firewall plugin need your support to spread the word about us.</p>
+		<p>By simply talking about our plugin and how effective it is for your site security, you lend us your support in return; You help other people find us; and of course, you help build a more secure WordPress community.</p>
+		<p>Please, could you leave us a 5* review?
+	</div>
+	<div class="notice-icon notice-icon-right">
+		<span class="dashicons dashicons-star-filled"></span>&nbsp;
+	</div>
+</div>
+
+<style type="text/css">
+	.notice-icon {
+		float: left;
+		height: 74px;
+		line-height: 176px;
+		min-width: 85px;
+		padding: 20px 0;
+	}
+	.notice-icon-right {
+		float: right;
+		width: 74px;
+	}
+	.notice-content {
+		float: left;
+		width: 70%;
+	}
+	.notice-icon .dashicons {
+		display: inline-block;
+		font-size: 75px;
+		line-height: 70px;
+	}
+</style>
