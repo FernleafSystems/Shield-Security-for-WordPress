86d58699535b4a3d3ce991e7b1e2c7db30b727df
Paul G <paul@icontrolwp.com>
2015-09-16 13:32:36 +0100

    improvements to restricted message display

--- templates/php/notices/certain-options-restricted.php
+++ templates/php/notices/certain-options-restricted.php
@@ -11,11 +11,12 @@
 	);
 
 	function disable_input( element, index, array ) {
-		console.log(element);
 		$oItem = jQuery( 'input[name=' + element + ']' );
 		$oItem.prop( 'disabled', true );
-		$oItem.parent().append(
-			'<div class="restricted-option">' +
+		$oParentTr = $oItem.parents('tr');
+		$oParentTr.addClass( 'restricted-option-row' );
+		$oItem.parents('td').append(
+			'<div style="clear:both"></div><div class="restricted-option">' +
 			'<span class="dashicons dashicons-lock"></span>' +
 			'<?php echo $strings['editing_restricted'];?>'+' <?php echo $strings['unlock_link'];?>' +
 			'</div>'
@@ -23,11 +24,14 @@
 	}
 </script>
 <style>
+	.restricted-option-row {}
 	.restricted-option {
 		background-color: rgba(255, 255, 255, 0.6);
-		border: 1px solid rgba(0, 0, 0, 0.2);
+		border: 1px solid;
+		border-radius: 3px;
 		clear: both;
-		display: block;
+		color: rgba(195, 71, 76, 0.9);
+		display: inline-block;
 		line-height: 22px;
 		margin: 2px 0 4px;
 		padding: 7px 8px 5px 6px;
