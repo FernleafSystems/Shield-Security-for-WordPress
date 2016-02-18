e439d28a48ab397605912ad85015d0d7948e63c4
Paul G <paul@icontrolwp.com>
2016-02-18 10:38:45 +0000

    finalized google captcha for comments

--- src/processors/commentsfilter_googlerecaptcha.php
+++ src/processors/commentsfilter_googlerecaptcha.php
@@ -5,7 +5,10 @@ if ( !class_exists( 'ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha', false 
 require_once( dirname(__FILE__).ICWP_DS.'base_wpsf.php' );
 
 class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Processor_BaseWpsf {
-
+	/**
+	 * @var string
+	 */
+	protected $sCommentStatus;
 	/**
 	 * @var string
 	 */
@@ -23,6 +26,9 @@ class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Proce
 		add_action( 'wp_enqueue_scripts',		array( $this, 'loadGoogleRecaptchaJs' ), 99 );
 		add_action( 'comment_form',				array( $this, 'printGoogleRecaptchaCheck' ) );
 		add_filter( 'preprocess_comment',		array( $this, 'doCommentChecking' ), 1, 1 );
+
+		add_filter( $this->getFeatureOptions()->doPluginPrefix( 'comments_filter_status' ), array( $this, 'getCommentStatus' ), 1 );
+		add_filter( $this->getFeatureOptions()->doPluginPrefix( 'comments_filter_status_explanation' ), array( $this, 'getCommentStatusExplanation' ), 1 );
 	}
 
 	public function loadGoogleRecaptchaJs() {
@@ -51,7 +57,7 @@ class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Proce
 		$oFO = $this->getFeatureOptions();
 		$sSiteKey = $oFO->getGoogleRecaptchaSiteKey();
 		return sprintf(
-			'%s<div class="g-recaptcha" data-sitekey="%s"></div>',
+			'%s<div class="g-recaptcha" data-sitekey="%s" style="margin: 10px 0;"></div>',
 			'<style>@media screen and (max-height: 575px){
 #rc-imageselect, .g-recaptcha iframe {transform:scale(0.90);-webkit-transform:scale(0.90);transform-origin:0 0;-webkit-transform-origin:0 0;}</style>',
 			$sSiteKey
@@ -94,6 +100,7 @@ class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Proce
 
 		if ( $bIsSpam ) {
 			$this->doStatIncrement( sprintf( 'spam.recaptcha.%s', $sStatKey ) );
+			$this->sCommentStatus = $this->getOption( 'comments_default_action_spam_bot' );
 			$this->setCommentStatusExplanation( $sExplanation );
 
 			// We now black mark this IP
@@ -109,6 +116,26 @@ class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Proce
 	}
 
 	/**
+	 * A private plugin filter that lets us return up the newly set comment status.
+	 *
+	 * @param $sCurrentCommentStatus
+	 * @return string
+	 */
+	public function getCommentStatus( $sCurrentCommentStatus ) {
+		return empty( $sCurrentCommentStatus )? $this->sCommentStatus : $sCurrentCommentStatus;
+	}
+
+	/**
+	 * A private plugin filter that lets us return up the newly set comment status explanation
+	 *
+	 * @param $sCurrentCommentStatusExplanation
+	 * @return string
+	 */
+	public function getCommentStatusExplanation( $sCurrentCommentStatusExplanation ) {
+		return empty( $sCurrentCommentStatusExplanation )? $this->sCommentStatusExplanation : $sCurrentCommentStatusExplanation;
+	}
+
+	/**
 	 * @param $sExplanation
 	 */
 	protected function setCommentStatusExplanation( $sExplanation ) {
@@ -116,7 +143,7 @@ class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Proce
 			'[* '.sprintf(
 				_wpsf__('%s plugin marked this comment as "%s".').' '._wpsf__( 'Reason: %s' ),
 				$this->getController()->getHumanName(),
-				$this->getOption( 'comments_default_action_spam_bot' ),
+				$this->sCommentStatus,
 				$sExplanation
 			)." *]\n";
 	}
