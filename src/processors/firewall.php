3f92bda2b7f784dbea9eeba817ea5ef0c0e38628
Paul G <paul@icontrolwp.com>
2016-02-26 09:20:19 +0000

    add the pwd to the white list

--- src/processors/firewall.php
+++ src/processors/firewall.php
@@ -438,6 +438,7 @@ if ( !class_exists( 'ICWP_WPSF_Processor_Firewall', false ) ):
 						'txn_id',
 						'_wp_http_referer',
 						'_wp_original_http_referer',
+						'pwd',
 						'url',
 						'referredby',
 						'redirect_to'
