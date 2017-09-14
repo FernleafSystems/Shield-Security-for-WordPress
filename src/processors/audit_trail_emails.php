7f2fb38a0cb5768e60c135d26a93d47baff14f15
Paul Goodchild <paul@icontrolwp.com>
2017-09-14 11:46:59 +0100

    reformat the auditing system

--- src/processors/audit_trail_emails.php
+++ src/processors/audit_trail_emails.php
@@ -4,16 +4,14 @@ if ( class_exists( 'ICWP_WPSF_Processor_AuditTrail_Emails' ) ) {
 	return;
 }
 
-require_once( dirname( __FILE__ ).DIRECTORY_SEPARATOR.'base_wpsf.php' );
+require_once( dirname( __FILE__ ).DIRECTORY_SEPARATOR.'audit_trail_base_auditor.php' );
 
-class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_Processor_BaseWpsf {
+class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_AuditTrail_Auditor_Base {
 
 	/**
 	 */
 	public function run() {
-		if ( $this->getIsOption( 'enable_audit_context_emails', 'Y' ) ) {
-			add_filter( 'wp_mail', array( $this, 'auditEmailSend' ) );
-		}
+		add_filter( 'wp_mail', array( $this, 'auditEmailSend' ) );
 	}
 
 	/**
@@ -25,19 +23,11 @@ class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_Processor_BaseWpsf
 		if ( is_array( $sTo ) ) {
 			$sTo = implode( ', ', $sTo );
 		}
-		$this->getAuditTrailEntries()
-			 ->add( 'emails', 'email_attempt_send', 1,
-				 sprintf( _wpsf__( 'There was an attempt to send an email using the "%s" function.' ), 'wp_mail' )
-				 .' '.sprintf( _wpsf__( 'It was sent to "%s" with the subject "%s".' ), $sTo, $aEmailParameters[ 'subject' ] )
-			 );
+		$this->add( 'emails', 'email_attempt_send', 1,
+			sprintf( _wpsf__( 'There was an attempt to send an email using the "%s" function.' ), 'wp_mail' )
+			.' '.sprintf( _wpsf__( 'It was sent to "%s" with the subject "%s".' ), $sTo, $aEmailParameters[ 'subject' ] )
+		);
 
 		return $aEmailParameters;
-	}
-
-	/**
-	 * @return ICWP_WPSF_AuditTrail_Entries
-	 */
-	protected function getAuditTrailEntries() {
-		return ICWP_WPSF_AuditTrail_Entries::GetInstance();
 	}
 }
