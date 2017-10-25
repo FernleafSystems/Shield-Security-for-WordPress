6fd4c2fe1d637dba6223cce7097927cfa89454c7
Paul Goodchild <gitkraken@paulgoodchild.net>
2017-10-25 13:12:44 +0100

    further ip detection improvement

--- src/features/plugin.php
+++ src/features/plugin.php
@@ -73,7 +73,7 @@ class ICWP_WPSF_FeatureHandler_Plugin extends ICWP_WPSF_FeatureHandler_BaseWpsf 
 		if ( !$this->isVisitorAddressSourceAutoDetect() ) {
 
 			$sIp = $oDp->FetchServer( $this->getVisitorAddressSource() );
-			if ( $oIpService->isViablePublicVisitorIp( $sIp ) ) {
+			if ( $oIpService->isViablePublicVisitorIp( $sIp, $this->getMyServerIp() ) ) {
 				$oIpService->setRequestIpAddress( $sIp );
 			}
 			else {
