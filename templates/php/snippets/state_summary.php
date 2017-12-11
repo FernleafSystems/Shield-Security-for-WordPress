00822ab154fe819a9a7679704018bd860a4d50b9
Paul Goodchild <paul@icontrolwp.com>
2017-12-11 15:06:15 +0000

    refined popover styles

--- templates/php/snippets/state_summary.php
+++ templates/php/snippets/state_summary.php
@@ -7,14 +7,19 @@ if ( empty( $aSummaryData ) ) {
 	<?php foreach ( $aSummaryData as $nKey => $aSummary ) : ?>
 		<div class="summary-state state-<?php echo $aSummary[ 'enabled' ] ? 'on' : 'off'; ?> <?php echo $aSummary[ 'active' ] ? 'active-feature' : ''; ?> "
 			 id="feature-<?php echo $aSummary[ 'slug' ]; ?>">
-			<a class="feature-icon" data-toggle="tooltip"
+			<a class="feature-icon"
 				<?php echo sprintf( 'href="%s"', $aSummary[ 'href' ] ); ?>
-			   title="<?php echo $aSummary[ 'name' ]; ?> : <?php echo $aSummary[ 'enabled' ] ? $strings[ 'on' ] : $strings[ 'off' ]; ?>"
+			   title="<?php echo $aSummary[ 'name' ]; ?>"
+			   data-content="<?php echo $aSummary[ 'enabled' ] ? $strings[ 'on' ] : $strings[ 'off' ]; ?>"
 			   style="display: block; text-align: center; width: 100%;"></a>
 		</div>
 	<?php endforeach; ?>
 	<div style="clear: both"></div>
 </div>
 <script type="text/javascript">
-	jQuery( 'a.feature-icon' ).tooltip( { placement: 'left' } );
+	jQuery( 'a.feature-icon' ).popover( {
+		placement: 'left',
+		trigger: 'hover'
+	} );
+	// jQuery( '#feature-plugin a.feature-icon' ).popover( 'show' );
 </script>
