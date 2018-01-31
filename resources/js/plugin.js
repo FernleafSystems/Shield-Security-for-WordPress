4439aa0f7fb9f5bcc08114e52cd228c70917ec6c
Paul Goodchild <paul@icontrolwp.com>
2018-01-31 18:38:04 +0000

    retain test JS

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1,6 +1,9 @@
 var iCWP_WPSF_OptionsPages = new function () {
 
 	var showWaiting = function ( event ) {
+		/* var $oLink = jQuery( this ); for the inner collapses
+		jQuery( '#' + $oLink.data( 'targetcollapse' ) ).collapse( 'show' ); */
+
 		iCWP_WPSF_BodyOverlay.show();
 	};
 
