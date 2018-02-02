7a6b3e48f068a0afb8bb494375bee9848e6aa9c8
Paul Goodchild <paul@icontrolwp.com>
2018-02-02 10:53:35 +0000

    replace name

--- src/features/admin_access_restriction.php
+++ src/features/admin_access_restriction.php
@@ -367,7 +367,9 @@ class ICWP_WPSF_FeatureHandler_AdminAccessRestriction extends ICWP_WPSF_FeatureH
 			case 'section_whitelabel' :
 				$sTitle = _wpsf__( 'Shield White Label' );
 				$aSummary = array(
-					sprintf( _wpsf__( 'Purpose - %s' ), _wpsf__( 'Rename and re-brand the Shield Security plugin for your client site installations.' ) ),
+					sprintf( _wpsf__( 'Purpose - %s' ),
+						sprintf( _wpsf__( 'Rename and re-brand the %s plugin for your client site installations.' ),
+							$this->getConn()->getHumanName() ) ),
 				);
 				$sTitleShort = _wpsf__( 'White Label' );
 				break;
