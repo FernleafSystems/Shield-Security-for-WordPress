7d720725fa4a4016f2d1138c3362cd5b804a779c
Paul G <paul@icontrolwp.com>
2018-04-28 14:50:30 +0100

    only enqueue recaptcha if the page has comments enabled

--- src/processors/commentsfilter_googlerecaptcha.php
+++ src/processors/commentsfilter_googlerecaptcha.php
@@ -14,11 +14,19 @@ class ICWP_WPSF_Processor_CommentsFilter_GoogleRecaptcha extends ICWP_WPSF_Proce
 		if ( !$oFO->getIsGoogleRecaptchaReady() ) {
 			return;
 		}
-
 		parent::run();
 
-		add_action( 'wp_enqueue_scripts', array( $this, 'registerGoogleRecaptchaJs' ), 99 );
-		add_action( 'comment_form_after_fields', array( $this, 'printGoogleRecaptchaCheck' ) );
+		add_action( 'wp', array( $this, 'setup' ) );
+	}
+
+	/**
+	 * The WP Query is alive and well at this stage so we can assume certain data is available.
+	 */
+	public function setup() {
+		if ( $this->loadWpCommentsProcessor()->isCommentsOpen() ) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'registerGoogleRecaptchaJs' ), 99 );
+			add_action( 'comment_form_after_fields', array( $this, 'printGoogleRecaptchaCheck' ) );
+		}
 	}
 
 	/**
