d3b6cce294b2a763dc99d5c9d7f2fea256894bb2
Paul G <paul@icontrolwp.com>
2018-07-31 12:04:39 +0100

    only update cooldown last attempt if attempt was valid

--- src/processors/loginprotect_cooldown.php
+++ src/processors/loginprotect_cooldown.php
@@ -17,7 +17,6 @@ class ICWP_WPSF_Processor_LoginProtect_Cooldown extends ICWP_WPSF_Processor_Logi
 
 			$bWithinCooldownPeriod = $this->isWithinCooldownPeriod();
 			$nRemaining = $this->getLoginCooldownInterval() - $this->getSecondsSinceLastLogin();
-			$this->updateLastLoginTime();
 
 			// At this point someone has attempted to login within the previous login wait interval
 			// So we remove WordPress's authentication filter and our own user check authentication
@@ -35,8 +34,9 @@ class ICWP_WPSF_Processor_LoginProtect_Cooldown extends ICWP_WPSF_Processor_Logi
 				throw new Exception( $sErrorString );
 			}
 			else {
-				$this->setFactorTested( true );
-				$this->doStatIncrement( 'login.cooldown.success' );
+				$this->updateLastLoginTime()
+					 ->setFactorTested( true )
+					 ->doStatIncrement( 'login.cooldown.success' );
 			}
 		}
 	}
