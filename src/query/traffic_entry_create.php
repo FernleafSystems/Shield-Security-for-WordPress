a56b307db42809d686d325c26cba693fe7fb7287
Paul G <paul@icontrolwp.com>
2018-08-06 18:15:18 +0100

    create a default created_at

--- src/query/traffic_entry_create.php
+++ src/query/traffic_entry_create.php
@@ -13,6 +13,9 @@ class ICWP_WPSF_Query_TrafficEntry_Create extends ICWP_WPSF_Query_Base {
 	 * @return bool|int
 	 */
 	public function create( ICWP_WPSF_TrafficEntryVO $oEntry ) {
+		if ( $oEntry->created_at < 1 ) {
+			$oEntry->created_at = $this->loadDP()->time();
+		}
 		$mResult = $this->loadDbProcessor()
 						->insertDataIntoTable( $this->getTable(), $oEntry->getRawDataAsArray() );
 		return ( $mResult === false ) ? false : $mResult > 0;
