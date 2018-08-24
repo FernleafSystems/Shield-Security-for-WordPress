7752009b140e1a72aa20740d36a15d94292708dd
Paul G <paul@icontrolwp.com>
2018-08-24 13:45:47 +0100

    refactor notes queries

--- src/query/notes/select.php
+++ src/query/notes/select.php
@@ -4,7 +4,7 @@ if ( class_exists( 'ICWP_WPSF_Query_PluginNotes_Select', false ) ) {
 	return;
 }
 
-require_once( dirname( __FILE__ ).'/base_select.php' );
+require_once( dirname( __DIR__ ).'/base_select.php' );
 
 class ICWP_WPSF_Query_PluginNotes_Select extends ICWP_WPSF_Query_BaseSelect {
 
