3ecbf5386d4db7b174c311ac9e8ac52a45a2f82f
Paul G <paul@icontrolwp.com>
2018-08-24 13:46:09 +0100

    refactor notes

--- src/query/notes/delete.php
+++ src/query/notes/delete.php
@@ -4,7 +4,7 @@ if ( class_exists( 'ICWP_WPSF_Query_PluginNotes_Delete', false ) ) {
 	return;
 }
 
-require_once( dirname( __FILE__ ).'/base_delete.php' );
+require_once( dirname( __DIR__ ).'/base_delete.php' );
 
 class ICWP_WPSF_Query_PluginNotes_Delete extends ICWP_WPSF_Query_BaseDelete {
 
@@ -12,7 +12,7 @@ class ICWP_WPSF_Query_PluginNotes_Delete extends ICWP_WPSF_Query_BaseDelete {
 	 * @return ICWP_WPSF_Query_PluginNotes_Count
 	 */
 	protected function getCounter() {
-		require_once( dirname( __FILE__ ).'/plugin_notes_count.php' );
+		require_once( __DIR__.'/count.php' );
 		$oCounter = new ICWP_WPSF_Query_PluginNotes_Count();
 		return $oCounter->setTable( $this->getTable() );
 	}
