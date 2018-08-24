b28cd37906f87790069c79bbda229537a94016d8
Paul G <paul@icontrolwp.com>
2018-08-24 13:33:40 +0100

    update to include all trait changes

--- src/query/traffic/common.php
+++ src/query/traffic/common.php
@@ -1,21 +1,78 @@
 <?php
 
-if ( class_exists( 'ICWP_WPSF_Query_TrafficEntry_Insert', false ) ) {
+if ( trait_exists( 'ICWP_WPSF_Query_TrafficEntry_Common', false ) ) {
 	return;
 }
 
-require_once( dirname( __DIR__ ).'/base_insert.php' );
-
-class ICWP_WPSF_Query_TrafficEntry_Insert extends ICWP_WPSF_Query_BaseInsert {
+trait ICWP_WPSF_Query_TrafficEntry_Common {
 
 	/**
-	 * @param ICWP_WPSF_TrafficEntryVO $oEntry
-	 * @return bool
+	 * Will test whether the Binary IP can be converted back before applying filter.
+	 * @param mixed $bBinaryIp - IP has already been converted using inet_pton
+	 * @return $this
 	 */
-	public function create( $oEntry ) {
-		if ( $oEntry->created_at < 1 ) {
-			$oEntry->created_at = $this->loadDP()->time();
+	public function filterByIp( $bBinaryIp ) {
+		if ( inet_ntop( $bBinaryIp ) !== false ) {
+			$this->addWhereEquals( 'ip', $bBinaryIp );
 		}
-		return $this->setInsertData( $oEntry->getRawDataAsArray() )->query() === 1;
+		return $this;
+	}
+
+	/**
+	 * Will test whether the Binary IP can be converted back before applying filter.
+	 * @param mixed $bBinaryIp - IP has already been converted using inet_pton
+	 * @return $this
+	 */
+	public function filterByNotIp( $bBinaryIp ) {
+		if ( inet_ntop( $bBinaryIp ) !== false ) {
+			$this->addWhere( 'ip', $bBinaryIp, '!=' );
+		}
+		return $this;
+	}
+
+	/**
+	 * @param bool $bIsLoggedIn - true is logged-in, false is not logged-in
+	 * @return $this
+	 */
+	public function filterByIsLoggedIn( $bIsLoggedIn ) {
+		return $this->addWhere( 'uid', 0, $bIsLoggedIn ? '>' : '=' );
+	}
+
+	/**
+	 * @param bool $bIsTransgression
+	 * @return $this
+	 */
+	public function filterByIsTransgression( $bIsTransgression ) {
+		return $this->addWhere( 'trans', 0, $bIsTransgression ? 1 : 0 );
+	}
+
+	/**
+	 * @param string $sTerm
+	 * @return $this
+	 */
+	public function filterByPathContains( $sTerm ) {
+		return $this->addWhereSearch( 'path', $sTerm );
+	}
+
+	/**
+	 * @param int $nId
+	 * @return $this
+	 */
+	public function filterByUserId( $nId ) {
+		return $this->addWhereEquals( 'uid', (int)$nId );
+	}
+
+	/**
+	 * @param string $sCode
+	 * @return $this
+	 */
+	public function filterByResponseCode( $sCode ) {
+		if ( is_numeric( $sCode ) ) {
+			$sCode = (string)$sCode;
+			if ( $sCode === '0' || preg_match( '#^[0-5]{1}[0-9]{2}$#', $sCode ) ) {
+				$this->addWhereEquals( 'code', $sCode );
+			}
+		}
+		return $this;
 	}
 }
--- src/query/traffic/count.php
+++ src/query/traffic/count.php
@@ -4,7 +4,10 @@ if ( class_exists( 'ICWP_WPSF_Query_TrafficEntry_Count', false ) ) {
 	return;
 }
 
-require_once( dirname( __FILE__ ).'/base_count.php' );
+require_once( __DIR__.'/common.php' );
+require_once( dirname( __DIR__ ).'/base_count.php' );
 
 class ICWP_WPSF_Query_TrafficEntry_Count extends ICWP_WPSF_Query_BaseCount {
+
+	use ICWP_WPSF_Query_TrafficEntry_Common;
 }
