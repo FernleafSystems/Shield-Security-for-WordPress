7d4ed01d32f5e439b225018477504de190332bcc
Paul G <paul@icontrolwp.com>
2018-09-11 14:11:59 +0100

    add whitelist exemptions

--- src/config/feature-email.php
+++ src/config/feature-email.php
@@ -1,36 +1,37 @@
 {
-  "slug": "email",
+  "slug":       "email",
   "properties": {
-    "slug": "email",
-    "name": "Email",
+    "slug":                  "email",
+    "name":                  "Email",
     "show_module_menu_item": false,
-    "auto_enabled":           true,
-    "storage_key": "email",
-    "show_central": false,
-    "premium": false,
-    "access_restricted": true
+    "auto_enabled":          true,
+    "storage_key":           "email",
+    "show_central":          false,
+    "premium":               false,
+    "access_restricted":     true,
+    "whitelist_exempt":      true
   },
-  "sections": [
+  "sections":   [
     {
-      "slug": "section_email_options",
-      "title": "Email Options",
+      "slug":    "section_email_options",
+      "title":   "Email Options",
       "primary": true
     },
     {
-      "slug": "section_non_ui",
+      "slug":   "section_non_ui",
       "hidden": true
     }
   ],
-  "options": [
+  "options":    [
     {
-      "key": "send_email_throttle_limit",
-      "section": "section_email_options",
-      "default": 10,
-      "type": "integer",
-      "link_info": "",
-      "link_blog": "",
-      "name": "Email Throttle Limit",
-      "summary": "Limit Emails Per Second",
+      "key":         "send_email_throttle_limit",
+      "section":     "section_email_options",
+      "default":     10,
+      "type":        "integer",
+      "link_info":   "",
+      "link_blog":   "",
+      "name":        "Email Throttle Limit",
+      "summary":     "Limit Emails Per Second",
       "description": "You throttle emails sent by this plugin by limiting the number of emails sent every second. This is useful in case you get hit by a bot attack. Zero (0) turns this off. Suggested: 10."
     }
   ]
