3aabe86f25083891986280ea95e3febd95027c54
Paul G <paul@icontrolwp.com>
2018-11-09 11:17:43 +0000

    streamline JS tables usage

--- resources/js/shield-tables.js
+++ resources/js/shield-tables.js
@@ -1,24 +1,5 @@
 jQuery.fn.icwpWpsfTableWithFilter = function ( aOptions ) {
 
-	var $oThis = this;
-	var aOpts = jQuery.extend( {}, aOptions );
-	/**
-	 * @var icwpWpsfAjaxTable
-	 */
-	var $oTable;
-	var $oForm;
-
-	initialise();
-
-	function initialise() {
-		$oTable = jQuery( aOpts[ 'selector_table_container' ] ).icwpWpsfAjaxTable( aOpts );
-		jQuery( document ).ready( function () {
-			$oForm = jQuery( aOpts[ 'selector_filter_form' ] );
-			$oForm.on( 'submit', submitFilters );
-			$oForm.on( 'click', 'a#ClearForm', resetFilters );
-		} );
-	}
-
 	var resetFilters = function ( evt ) {
 		jQuery( 'input[type=text]', $oForm ).each( function () {
 			jQuery( this ).val( '' );
@@ -29,14 +10,27 @@ jQuery.fn.icwpWpsfTableWithFilter = function ( aOptions ) {
 		jQuery( 'input[type=checkbox]', $oForm ).each( function () {
 			jQuery( this ).prop( 'checked', false );
 		} );
-		$oTable.renderTableFromForm( $oForm );
+		aOpts[ 'table' ].renderTableFromForm( $oForm );
 	};
 
 	var submitFilters = function ( evt ) {
 		evt.preventDefault();
-		$oTable.renderTableFromForm( $oForm );
+		aOpts[ 'table' ].renderTableFromForm( $oForm );
 		return false;
 	};
+
+	var initialise = function () {
+		jQuery( document ).ready( function () {
+			$oForm = jQuery( aOpts[ 'selector_filter_form' ] );
+			$oForm.on( 'submit', submitFilters );
+			$oForm.on( 'click', 'a#ClearForm', resetFilters );
+		} );
+	};
+
+	var $oThis = this;
+	var aOpts = jQuery.extend( {}, aOptions );
+	var $oForm;
+	initialise();
 
 	return this;
 };
