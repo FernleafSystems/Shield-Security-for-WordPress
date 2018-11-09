16339eaed02bd9a100297209ea348755aa165cf3
Paul G <paul@icontrolwp.com>
2018-11-09 17:21:16 +0000

    Add SQL comments

--- src/processors/traffic_logger.php
+++ src/processors/traffic_logger.php
@@ -184,14 +184,14 @@ class ICWP_WPSF_Processor_TrafficLogger extends ICWP_WPSF_BaseDbProcessor {
 	protected function getCreateTableSql() {
 		$sSqlTables = "CREATE TABLE %s (
 			id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-			rid varchar(10) NOT NULL DEFAULT '',
-			uid int(11) UNSIGNED NOT NULL DEFAULT 0,
-			ip varbinary(16) DEFAULT NULL,
-			path text NOT NULL DEFAULT '',
-			code int(5) NOT NULL DEFAULT '200',
-			verb varchar(10) NOT NULL DEFAULT 'get',
-			ua text,
-			trans tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+			rid varchar(10) NOT NULL DEFAULT '' COMMENT 'Request ID',
+			uid int(11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'User ID',
+			ip varbinary(16) DEFAULT NULL COMMENT 'Visitor IP Address',
+			path text NOT NULL DEFAULT '' COMMENT 'Request Path or URI',
+			code int(5) NOT NULL DEFAULT '200' COMMENT 'HTTP Response Code',
+			verb varchar(10) NOT NULL DEFAULT 'get' COMMENT 'HTTP Method',
+			ua text COMMENT 'Browser User Agent String',
+			trans tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Trangression',
 			created_at int(15) UNSIGNED NOT NULL DEFAULT 0,
 			deleted_at int(15) UNSIGNED NOT NULL DEFAULT 0,
  			PRIMARY KEY  (id)
