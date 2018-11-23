1eca32f425e75b1bfc132d6db24fff7d6d536fc3
Paul G <paul@icontrolwp.com>
2018-11-23 18:46:09 +0000

    removes core file scan repair links

--- src/processors/hackprotect_scan_wcf.php
+++ src/processors/hackprotect_scan_wcf.php
@@ -13,32 +13,6 @@ class ICWP_WPSF_Processor_HackProtect_Wcf extends ICWP_WPSF_Processor_ScanBase {
 	const SCAN_SLUG = 'wcf';
 
 	/**
-	 */
-	public function run() {
-		parent::run();
-
-		if ( $this->loadWpUsers()->isUserAdmin() ) {
-			$oReq = $this->loadRequest();
-
-			switch ( $oReq->query( 'shield_action' ) ) {
-
-				case 'repair_file':
-					$sPath = '/'.trim( $oReq->query( 'repair_file_path' ) ); // "/" prevents esc_url() from prepending http.
-					$sMd5FilePath = urldecode( esc_url( $sPath ) );
-					if ( !empty( $sMd5FilePath ) ) {
-						if ( $this->repairCoreFile( $sMd5FilePath ) ) {
-							$this->getMod()
-								 ->setFlashAdminNotice( _wpsf__( 'File was successfully replaced with an original from WordPress.org' ) );
-						}
-						else {
-							$this->getMod()->setFlashAdminNotice( _wpsf__( 'File was not replaced' ), true );
-						}
-					}
-			}
-		}
-	}
-
-	/**
 	 * @param Shield\Scans\WpCore\ResultsSet $oResults
 	 * @return \FernleafSystems\Wordpress\Plugin\Shield\Databases\Scanner\EntryVO[]
 	 */
@@ -183,11 +157,10 @@ class ICWP_WPSF_Processor_HackProtect_Wcf extends ICWP_WPSF_Processor_ScanBase {
 		$aContent = array(
 			sprintf( _wpsf__( "The %s Core File Scanner found files with potential problems." ), $sName ),
 			sprintf( '%s: %s', _wpsf__( 'Site URL' ), sprintf( '<a href="%s" target="_blank">%s</a>', $sHomeUrl, $sHomeUrl ) ),
-			''
 		);
 
 		if ( $oFO->isWcfScanAutoRepair() || $oFO->isIncludeFileLists() ) {
-			$aContent = $this->buildListOfFilesForEmail( $oResults );
+			$aContent = array_merge( $aContent, $this->buildListOfFilesForEmail( $oResults ) );
 			$aContent[] = '';
 
 			if ( $oFO->isWcfScanAutoRepair() ) {
@@ -199,14 +172,14 @@ class ICWP_WPSF_Processor_HackProtect_Wcf extends ICWP_WPSF_Processor_ScanBase {
 				$aContent[] = _wpsf__( 'Alternatively you can have the plugin attempt to repair/replace these files automatically.' )
 							  .' [<a href="https://icwp.io/moreinfochecksum">'._wpsf__( 'More Info' ).']</a>';
 			}
-			$aContent[] = '';
 		}
 
+		$aContent[] = '';
 		$aContent[] = _wpsf__( 'We recommend you run the scanner to review your site' ).':';
 		$aContent[] = $this->getScannerButtonForEmail();
-		$aContent[] = '';
 
 		if ( !$oFO->getConn()->isRelabelled() ) {
+			$aContent[] = '';
 			$aContent[] = '[ <a href="https://icwp.io/moreinfochecksum">'._wpsf__( 'More Info On This Scanner' ).' ]</a>';
 		}
 
@@ -221,15 +194,17 @@ class ICWP_WPSF_Processor_HackProtect_Wcf extends ICWP_WPSF_Processor_ScanBase {
 		$aContent = array();
 
 		if ( $oResult->hasChecksumFailed() ) {
-			$aContent[] = _wpsf__( "The contents of the core files listed below don't match official WordPress files:" );
+			$aContent[] = '';
+			$aContent[] = _wpsf__( "The following files have different content:" );
 			foreach ( $oResult->getChecksumFailedPaths() as $sFile ) {
-				$aContent[] = ' - '.$sFile.$this->getFileRepairLink( $sFile );
+				$aContent[] = ' - '.$sFile;
 			}
 		}
 		if ( $oResult->hasMissing() ) {
-			$aContent[] = _wpsf__( 'The WordPress Core Files listed below are missing:' );
+			$aContent[] = '';
+			$aContent[] = _wpsf__( 'The following files are missing:' );
 			foreach ( $oResult->getMissingPaths() as $sFile ) {
-				$aContent[] = ' - '.$sFile.$this->getFileRepairLink( $sFile );
+				$aContent[] = ' - '.$sFile;
 			}
 		}
 		return $aContent;
@@ -239,20 +214,9 @@ class ICWP_WPSF_Processor_HackProtect_Wcf extends ICWP_WPSF_Processor_ScanBase {
 	 * @param string $sFile
 	 * @return string
 	 */
-	protected function getFileRepairLink( $sFile ) {
-		return sprintf( ' ( <a href="%s">%s</a> / <a href="%s">%s</a> )',
-			add_query_arg(
-				array(
-					'shield_action'    => 'repair_file',
-					'repair_file_path' => urlencode( $sFile )
-				),
-				$this->loadWp()->getUrl_WpAdmin()
-			),
-			_wpsf__( 'Repair file now' ),
-			$this->getMod()->getDef( 'url_wordress_core_svn' )
-			.'tags/'.$this->loadWp()->getVersion().'/'.$sFile,
-			_wpsf__( 'WordPress.org source file' )
-		);
+	private function getWpFileDownloadUrl( $sFile ) {
+		return $this->getMod()->getDef( 'url_wordress_core_svn' )
+			   .'tags/'.$this->loadWp()->getVersion().'/'.$sFile;
 	}
 
 	/**
