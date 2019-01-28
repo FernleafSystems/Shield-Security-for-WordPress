3690c33e7502aae92ebe15fd809b30280c45ca4b
Paul G <paul@icontrolwp.com>
2019-01-28 15:36:40 +0000

    update versions

--- plugin-spec.php
+++ plugin-spec.php
@@ -1,7 +1,7 @@
 {
   "properties":   {
-    "version":                 "7.0.1",
-    "release_timestamp":       1548682000,
+    "version":                 "7.0.2",
+    "release_timestamp":       1548689761,
     "slug_parent":             "icwp",
     "slug_plugin":             "wpsf",
     "human_name":              "Shield",
