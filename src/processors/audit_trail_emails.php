14f9a3658d205a0d690bda51220adafbe0f70e63
Paul G <paul@icontrolwp.com>
2019-02-05 11:37:33 +0000

    try to capture B/CC in email audit trail

--- src/processors/audit_trail_emails.php
+++ src/processors/audit_trail_emails.php
@@ -1,11 +1,13 @@
 <?php
 
+use FernleafSystems\Wordpress\Services\Services;
+
 class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_AuditTrail_Auditor_Base {
 
 	/**
 	 */
 	public function run() {
-		add_filter( 'wp_mail', array( $this, 'auditEmailSend' ) );
+		add_filter( 'wp_mail', array( $this, 'auditEmailSend' ), PHP_INT_MAX );
 	}
 
 	/**
@@ -14,6 +16,7 @@ class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_AuditTrail_Auditor
 	 */
 	public function auditEmailSend( $aEmail ) {
 
+		$aMsg = [];
 		if ( is_array( $aEmail ) ) {
 			$sTo = isset( $aEmail[ 'to' ] ) ? $aEmail[ 'to' ] : 'no email address provided';
 			if ( is_array( $sTo ) ) {
@@ -26,6 +29,25 @@ class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_AuditTrail_Auditor
 				sprintf( _wpsf__( 'There was an attempt to send an email using the "%s" function.' ), 'wp_mail' ),
 				sprintf( _wpsf__( 'It was sent to "%s" with the subject "%s".' ), $sTo, $aEmail[ 'subject' ] ),
 			);
+
+			// Attempt to capture BCC/CC
+			if ( !empty( $aEmail[ 'headers' ] ) ) {
+				$aHeaders = $aEmail[ 'headers' ];
+				if ( is_string( $aHeaders ) ) {
+					$aHeaders = explode( "\n", $aHeaders );
+				}
+				if ( is_array( $aHeaders ) ) {
+					$aCCs = $this->extractCcFromHeaders( $aHeaders );
+					if ( !empty( $aCCs[ 'bcc' ] ) ) {
+						$aMsg[] = sprintf( "BCC'd: %s.", implode( ', ', $aCCs[ 'bcc' ] ) );
+					}
+					if ( !empty( $aCCs[ 'cc' ] ) ) {
+						$aMsg[] = sprintf( "CC'd: %s.", implode( ', ', $aCCs[ 'cc' ] ) );
+					}
+				}
+			}
+
+			// Where was the wp_mail function called from
 			if ( !empty( $aBacktrace ) ) {
 				$aMsg[] = sprintf( _wpsf__( 'The "%s" function was called from the file "%s" on line %s.' ),
 					'wp_mail',
@@ -35,12 +57,47 @@ class ICWP_WPSF_Processor_AuditTrail_Emails extends ICWP_WPSF_AuditTrail_Auditor
 			}
 		}
 		else {
-			$aMsg = array( sprintf( _wpsf__( 'Attempting to log email, but data was not of the correct type (%s)' ), 'array' ) );
+			$aMsg[] = sprintf( _wpsf__( 'Attempting to log email, but data was not of the correct type (%s)' ), 'array' );
 		}
 
 		$this->add( 'emails', 'email_attempt_send', 1, implode( " ", $aMsg ) );
 
 		return $aEmail;
+	}
+
+	/**
+	 * @param array $aHeaders
+	 * @return array
+	 */
+	private function extractCcFromHeaders( $aHeaders ) {
+		$aCCs = array(
+			'bcc' => [],
+			'cc'  => []
+		);
+
+		$aHeaders = array_filter( array_map( 'trim', array_map( 'strtolower', $aHeaders ) ) );
+		foreach ( $aHeaders as $sHeader ) {
+			if ( preg_match( '#^\s*b?cc\s*:.+#i', $sHeader ) ) {
+				list( $sHead, $sEmails ) = explode( ':', str_replace( ' ', '', $sHeader ), 2 );
+				if ( strpos( ',', $sEmails ) !== false ) {
+					$aEmails = explode( ',', $sEmails );
+				}
+				else {
+					$aEmails = [ $sEmails ];
+				}
+
+				if ( isset( $aCCs[ $sHead ] ) ) {
+					$aCCs[ $sHead ][] = array_unique( array_merge(
+						$aCCs[ $sHead ],
+						array_filter( $aEmails,
+							function ( $sEmail ) {
+								return \FernleafSystems\Wordpress\Services\Services::Data()->validEmail( $sEmail );
+							} )
+					) );
+				}
+			}
+		}
+		return $aCCs;
 	}
 
 	/**
