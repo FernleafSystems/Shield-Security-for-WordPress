9d50c8735bd9527025492e4bf88a08966c529f72
Paul G <paul@icontrolwp.com>
2019-02-19 16:34:11 +0000

    build collate

--- src/lib/src/ChangeTrack/Snapshot/Collate.php
+++ src/lib/src/ChangeTrack/Snapshot/Collate.php
@@ -2,9 +2,22 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\ChangeTrack\Snapshot;
 
+/**
+ * Class Collate
+ * @package FernleafSystems\Wordpress\Plugin\Shield\ChangeTrack\Snapshot
+ */
 class Collate {
 
+	/**
+	 * @return array
+	 */
 	public function run() {
+		return [
+			'users'   => ( new BuildUsers() )->run(),
+			'post'    => ( new BuildPosts() )->run(),
+			'pages'   => ( new BuildPages() )->run(),
+			'plugins' => ( new BuildPlugins() )->run(),
+			'themes'  => ( new BuildThemes() )->run(),
+		];
 	}
-
 }
