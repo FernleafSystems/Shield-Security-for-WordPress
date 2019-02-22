a23dc0cb68559d6629917ab78fa3350fc353f299
Paul G <paul@icontrolwp.com>
2019-02-22 17:50:06 +0000

    support user blocking on profile edit

--- src/processors/usermanagement_suspend.php
+++ src/processors/usermanagement_suspend.php
@@ -9,6 +9,8 @@ class ICWP_WPSF_Processor_UserManagement_Suspend extends ICWP_WPSF_Processor_Bas
 		/** @var ICWP_WPSF_FeatureHandler_UserManagement $oFO */
 		$oFO = $this->getMod();
 		if ( $oFO->isSuspendManualEnabled() ) {
+			add_action( 'edit_user_profile', array( $this, 'addUserBlockOption' ) );
+			add_action( 'edit_user_profile_update', array( $this, 'handleUserBlockOptionSubmit' ) );
 			( new Suspend\Suspended() )
 				->setCon( $this->getCon() )
 				->run();
@@ -24,6 +26,42 @@ class ICWP_WPSF_Processor_UserManagement_Suspend extends ICWP_WPSF_Processor_Bas
 				->setMaxPasswordAge( $oFO->getPassExpireTimeout() )
 				->setCon( $this->getCon() )
 				->run();
+		}
+	}
+
+	/**
+	 * @param \WP_User $oUser
+	 */
+	public function addUserBlockOption( $oUser ) {
+		$oCon = $this->getCon();
+		$oMeta = $oCon->getUserMeta( $oUser );
+
+		if ( $oCon->isPluginAdmin() ) {
+			$aData = [
+				'user_to_edit_is_admin' => Services::WpUsers()->isUserAdmin( $oUser ),
+				'strings'               => array(
+					'title'       => _wpsf__( 'Suspend Account' ),
+					'label'       => _wpsf__( 'Check to block login by user' ),
+					'description' => _wpsf__( 'The user will never be able to login when their account is suspended.' ),
+				),
+				'vars'                  => [
+					'form_field'   => 'shield_suspend_user',
+					'is_suspended' => $oMeta->is_hard_suspended
+				]
+			];
+			echo $this->getMod()->renderTemplate( '/snippets/user/profile/suspend.twig', $aData, true );
+		}
+	}
+
+	/**
+	 * @param int $nUserId
+	 */
+	public function handleUserBlockOptionSubmit( $nUserId ) {
+		$oCon = $this->getCon();
+		$oMeta = $oCon->getUserMeta( Services::WpUsers()->getUserById( $nUserId ) );
+
+		if ( $oCon->isPluginAdmin() ) {
+			$oMeta->is_hard_suspended = Services::Request()->post( 'shield_suspend_user' ) === 'Y';
 		}
 	}
 }
