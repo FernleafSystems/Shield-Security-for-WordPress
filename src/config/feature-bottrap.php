8abefd9978d10107ebda49fdf958f8958c75d320
Paul G <paul@icontrolwp.com>
2019-03-10 11:37:13 +0000

    extend wpcli flag to bottrap

--- src/config/feature-bottrap.php
+++ src/config/feature-bottrap.php
@@ -12,6 +12,7 @@
     "premium":               true,
     "run_if_whitelisted":    false,
     "run_if_verified_bot":   false,
+    "run_if_wpcli":          false,
     "order":                 30
   },
   "sections":    [
