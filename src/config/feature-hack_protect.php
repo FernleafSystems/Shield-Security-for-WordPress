28360bd94b04c48ae2c1f5281cfb5caffd5eeb40
Paul G <paul@icontrolwp.com>
2019-03-21 08:22:44 +0000

    make abandoned plugin non-premium only

--- src/config/feature-hack_protect.php
+++ src/config/feature-hack_protect.php
@@ -176,7 +176,6 @@
     {
       "key":           "enabled_scan_apc",
       "section":       "section_scan_apc",
-      "premium":       true,
       "default":       "enabled_email",
       "type":          "select",
       "value_options": [
