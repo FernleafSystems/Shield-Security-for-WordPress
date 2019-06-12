15f5617c9640e3dbb7942ae49fdbac7f00de0dd0
Paul G <paul@icontrolwp.com>
2019-06-12 13:46:52 +0100

    complete strings to reinstall dialogs

--- src/processors/hackprotect_scan_ptg.php
+++ src/processors/hackprotect_scan_ptg.php
@@ -172,21 +172,30 @@ class ICWP_WPSF_Processor_HackProtect_Ptg extends ICWP_WPSF_Processor_HackProtec
 	}
 
 	public function printPluginReinstallDialogs() {
-		$aRenderData = [
-			'strings'     => [
-				'editing_restricted' => __( 'Editing this option is currently restricted.', 'wp-simple-firewall' ),
-				'download'           => sprintf(
-					__( 'For best security practices, %s will download and re-install the latest available version of this plugin.', 'wp-simple-firewall' ),
-					$this->getCon()->getHumanName()
-				)
+		echo $this->getMod()->renderTemplate(
+			'snippets/dialog_plugins_reinstall.twig',
+			[
+				'strings'     => [
+					'are_you_sure'       => __( 'Are you sure?', 'wp-simple-firewll' ),
+					'really_reinstall'   => __( 'Really Re-Install Plugin', 'wp-simple-firewll' ),
+					'wp_reinstall'       => __( 'WordPress will now download and install the latest available version of this plugin.', 'wp-simple-firewll' ),
+					'in_case'            => sprintf( '%s: %s',
+						__( 'Note', 'wp-simple-firewall' ),
+						__( 'In case of possible failure, it may be better to do this while the plugin is inactive.', 'wp-simple-firewll' )
+					),
+					'reinstall_first'    => __( 'Re-install first?', 'wp-simple-firewall' ),
+					'corrupted'          => __( "This ensures files for this plugin haven't been corrupted in any way.", 'wp-simple-firewall' ),
+					'choose'             => __( "You can choose to 'Activate Only' (not recommended), or close this message to cancel activation.", 'wp-simple-firewall' ),
+					'editing_restricted' => __( 'Editing this option is currently restricted.', 'wp-simple-firewall' ),
+					'download'           => sprintf(
+						__( 'For best security practices, %s will download and re-install the latest available version of this plugin.', 'wp-simple-firewall' ),
+						$this->getCon()->getHumanName()
+					)
+				],
+				'js_snippets' => []
 			],
-			'js_snippets' => []
-		];
-		echo $this->getMod()
-				  ->renderTemplate(
-					  'snippets/hg-plugins-reinstall-dialogs.php',
-					  $aRenderData
-				  );
+			true
+		);
 	}
 
 	/**
