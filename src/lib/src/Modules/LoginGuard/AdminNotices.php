dac8385c3902255556a6897ea608c587c705897e
Paul Goodchild <paul@icontrolwp.com>
2019-07-06 12:16:51 +0100

    add force off notice

--- src/lib/src/Modules/LoginGuard/AdminNotices.php
+++ src/lib/src/Modules/LoginGuard/AdminNotices.php
@@ -55,41 +55,4 @@ class AdminNotices extends Shield\Modules\Base\AdminNotices {
 			]
 		];
 	}
-
-	/**
-	 * @param Shield\Utilities\AdminNotices\NoticeVO $oNotice
-	 */
-	private function buildNoticeAdminUsersRestricted( $oNotice ) {
-		$oMod = $this->getMod();
-		/** @var Options $oOpts */
-		$oOpts = $oMod->getOptions();
-		$sName = $this->getCon()->getHumanName();
-
-		$oNotice->display = in_array(
-			Services::WpPost()->getCurrentPage(), $oOpts->getDef( 'restricted_pages_users' )
-		);
-
-		$oNotice->render_data = [
-			'notice_attributes' => [], // TODO
-			'strings'           => [
-				'title'          => sprintf( __( '%s Security Restrictions Applied', 'wp-simple-firewall' ), $sName ),
-				'notice_message' => __( 'Editing existing administrators, promoting existing users to the administrator role, or deleting administrator users is currently restricted.', 'wp-simple-firewall' )
-									.' '.__( 'Please authenticate with the Security Admin system before attempting any administrator user modifications.', 'wp-simple-firewall' ),
-				'unlock_link'    => sprintf(
-					'<a href="%1$s" title="%2$s" class="thickbox">%3$s</a>',
-					'#TB_inline?width=400&height=180&inlineId=WpsfAdminAccessLogin',
-					__( 'Security Admin Login', 'wp-simple-firewall' ),
-					__( 'Unlock Now', 'wp-simple-firewall' )
-				),
-			],
-			'hrefs'             => [
-				'setting_page' => sprintf(
-					'<a href="%s" title="%s">%s</a>',
-					$oMod->getUrl_AdminPage(),
-					__( 'Security Admin Login', 'wp-simple-firewall' ),
-					sprintf( __( 'Go here to manage settings and authenticate with the %s plugin.', 'wp-simple-firewall' ), $sName )
-				)
-			]
-		];
-	}
 }
