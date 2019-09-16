cfb881192b0b1722d3ba8b4cb24a73b7f55b81fe
Paul G <paul@icontrolwp.com>
2019-09-16 12:07:45 +0100

    use new WP Hashes API for vulnerabilities

--- src/lib/src/Scans/Wpv/Scan.php
+++ src/lib/src/Scans/Wpv/Scan.php
@@ -3,6 +3,8 @@
 namespace FernleafSystems\Wordpress\Plugin\Shield\Scans\Wpv;
 
 use FernleafSystems\Wordpress\Plugin\Shield;
+use FernleafSystems\Wordpress\Services\Services;
+use FernleafSystems\Wordpress\Services\Utilities\Vulnerabilities;
 
 class Scan extends Shield\Scans\Base\BaseScan {
 
@@ -13,7 +15,7 @@ class Scan extends Shield\Scans\Base\BaseScan {
 
 		$oCopier = new Shield\Scans\Helpers\CopyResultsSets();
 		foreach ( $oAction->items as $sFile => $sContext ) {
-			$oNewRes = $this->getItemScanner( $sContext )->scan( $sFile );
+			$oNewRes = $this->scanItem( $sContext, $sFile );
 			if ( $oNewRes instanceof Shield\Scans\Base\BaseResultsSet ) {
 				$oCopier->copyTo( $oNewRes, $oTempRs );
 			}
@@ -30,14 +32,46 @@ class Scan extends Shield\Scans\Base\BaseScan {
 
 	/**
 	 * @param string $sContext
-	 * @return PluginScanner|ThemeScanner
+	 * @param string $sFile
+	 * @return ResultsSet
 	 */
-	protected function getItemScanner( $sContext ) {
+	protected function scanItem( $sContext, $sFile ) {
+		$oResultsSet = new ResultsSet();
+
 		if ( $sContext == 'plugins' ) {
-			return ( new PluginScanner() )->setScanActionVO( $this->getScanActionVO() );
+			$oWpPlugins = Services::WpPlugins();
+			$sSlug = $oWpPlugins->getSlug( $sFile );
+			if ( empty( $sSlug ) ) {
+				$sSlug = dirname( $sFile );
+			}
+			$sVersion = $oWpPlugins->getPluginAsVo( $sFile )->Version;
+			$oLookup = new Vulnerabilities\Plugin();
 		}
 		else {
-			return ( new ThemeScanner() )->setScanActionVO( $this->getScanActionVO() );
+			$sSlug = $sFile;
+			$sVersion = Services::WpThemes()->getTheme( $sSlug )->get( 'Version' );
+			$oLookup = new Vulnerabilities\Theme();
 		}
+
+		$aVulns = $oLookup->getVulnerabilities( $sSlug, $sVersion );
+		$aVulns = array_filter( array_map(
+			function ( $aVuln ) {
+				return empty( $aVuln ) ? null
+					: ( new Shield\Scans\Wpv\WpVulnDb\WpVulnVO() )->applyFromArray( $aVuln );
+			},
+			( is_array( $aVulns ) ? $aVulns : [] )
+		) );
+
+		/** @var Shield\Scans\Wpv\WpVulnDb\WpVulnVO[] $aVulns */
+		foreach ( $aVulns as $oVo ) {
+			$oItem = new ResultItem();
+			$oItem->slug = $sFile;
+			$oItem->context = $sContext;
+			$oItem->wpvuln_id = $oVo->id;
+			$oItem->wpvuln_vo = $oVo->getRawDataAsArray();
+			$oResultsSet->addItem( $oItem );
+		}
+
+		return $oResultsSet;
 	}
 }
