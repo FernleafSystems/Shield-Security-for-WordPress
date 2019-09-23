213631289b4501f2f44e0ec9497144e8ae85da82
Paul G <paul@icontrolwp.com>
2019-09-23 11:20:09 +0100

    restrict malware scan to only WordPress directories

--- src/lib/src/Scans/Mal/ScanActionVO.php
+++ src/lib/src/Scans/Mal/ScanActionVO.php
@@ -8,7 +8,7 @@ use FernleafSystems\Wordpress\Plugin\Shield\Scans\Base\BaseScanActionVO;
  * Class ScanActionVO
  * @package FernleafSystems\Wordpress\Plugin\Shield\Scans\Mal
  * @property string[]   $file_exts
- * @property string[]   $scan_root_dir
+ * @property string[]   $scan_root_dirs
  * @property string[]   $paths_whitelisted
  * @property string[]   $patterns_regex
  * @property string[]   $patterns_simple
