c921263fdc05a3fd71f4e5ee4ba00f6f5975706e
Paul G <paul@icontrolwp.com>
2020-01-28 09:10:17 +0000

    update readme changelog

--- plugin-spec.php
+++ plugin-spec.php
@@ -1,8 +1,8 @@
 {
   "properties":   {
     "version":                 "8.5.2",
-    "release_timestamp":       1580207808,
-    "build":                   "202001.2702",
+    "release_timestamp":       1580202588,
+    "build":                   "202001.2801",
     "slug_parent":             "icwp",
     "slug_plugin":             "wpsf",
     "human_name":              "Shield",
