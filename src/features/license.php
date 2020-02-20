6fed2455baed9ef8dcb49a2788ecd2dd6dffd817
Paul G <paul@icontrolwp.com>
2020-02-20 14:00:29 +0000

    simplify premium

--- src/features/license.php
+++ src/features/license.php
@@ -13,11 +13,6 @@ class ICWP_WPSF_FeatureHandler_License extends ICWP_WPSF_FeatureHandler_BaseWpsf
 		);
 	}
 
-	protected function setupCustomHooks() {
-		parent::setupCustomHooks();
-		add_filter( $this->getCon()->getPremiumLicenseFilterName(), [ $this, 'hasValidWorkingLicense' ], PHP_INT_MAX );
-	}
-
 	/**
 	 * @return bool
 	 */
