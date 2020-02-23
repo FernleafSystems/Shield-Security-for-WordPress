ec04210e32c3b627693be2125eb372dadeb3b024
Paul G <paul@icontrolwp.com>
2020-02-23 14:00:34 +0000

    remove unnecessary cron parameter

--- src/processors/license.php
+++ src/processors/license.php
@@ -11,7 +11,7 @@ class ICWP_WPSF_Processor_License extends Modules\BaseShield\ShieldProcessor {
 		$oOpts = $this->getOptions();
 		$oReq = Services::Request();
 
-		// performs the license check
+		// performs the license check on-demand
 		add_action( $oCon->prefix( 'adhoc_cron_license_check' ), function () {
 			/** @var \ICWP_WPSF_FeatureHandler_License $oMod */
 			$oMod = $this->getMod();
@@ -31,7 +31,7 @@ class ICWP_WPSF_Processor_License extends Modules\BaseShield\ShieldProcessor {
 
 			case 'license_check':
 				if ( !wp_next_scheduled( $oCon->prefix( 'adhoc_cron_license_check' ) ) ) {
-					wp_schedule_single_event( $oReq->ts() + 20, $oCon->prefix( 'adhoc_cron_license_check' ), [ true ] );
+					wp_schedule_single_event( $oReq->ts() + 20, $oCon->prefix( 'adhoc_cron_license_check' ) );
 				}
 				break;
 		}
