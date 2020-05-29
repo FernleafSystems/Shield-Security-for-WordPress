965b35a051a6f6e7b0354547af484e5b4d5b3119
Paul G <paul@icontrolwp.com>
2020-05-29 12:11:15 +0100

    add updated email checking parts

--- src/features/user_management.php
+++ src/features/user_management.php
@@ -55,16 +55,21 @@ class ICWP_WPSF_FeatureHandler_UserManagement extends \ICWP_WPSF_FeatureHandler_
 
 		{
 			$aChecks = $oOpts->getEmailValidationChecks();
-			if ( !in_array( 'syntax', $aChecks ) ) {
+			if ( !empty( $aChecks ) ) {
 				$aChecks[] = 'syntax';
 			}
-			// fill in dependencies
-			if ( in_array( 'nondisposable', $aChecks ) && !in_array( 'mx', $aChecks ) ) {
-				$aChecks[] = 'mx';
-			}
-			if ( in_array( 'mx', $aChecks ) && !in_array( 'domain', $aChecks ) ) {
-				$aChecks[] = 'domain';
-			}
+			$oOpts->setOpt( 'email_checks', array_unique( $aChecks ) );
+		}
+	}
+
+	protected function updateHandler() {
+		/** @var UserManagement\Options $oOpts */
+		$oOpts = $this->getOptions();
+
+		$aChecks = $oOpts->getEmailValidationChecks();
+		if ( in_array( 'domain', $aChecks ) ) {
+			$aChecks[] = 'domain_registered';
+			unset( $aChecks[ array_search( 'domain', $aChecks ) ] );
 			$oOpts->setOpt( 'email_checks', $aChecks );
 		}
 	}
