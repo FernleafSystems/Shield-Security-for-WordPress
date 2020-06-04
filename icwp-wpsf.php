7aee7d91744dd453076383993be246ec421cf73d
Paul G <paul@icontrolwp.com>
2020-06-04 12:27:29 +0100

    update version

--- icwp-wpsf.php
+++ icwp-wpsf.php
@@ -3,7 +3,7 @@
  * Plugin Name: Shield Security
  * Plugin URI: https://shsec.io/2f
  * Description: Powerful, Easy-To-Use #1 Rated WordPress Security System
- * Version: 9.0.2
+ * Version: 9.0.3
  * Text Domain: wp-simple-firewall
  * Domain Path: /languages
  * Author: Shield Security
