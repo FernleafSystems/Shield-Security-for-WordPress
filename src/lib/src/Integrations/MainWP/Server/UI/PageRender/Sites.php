38d9ebb35b619a6cfc585db64587ddd52a6a52c8
Paulie G <paul@icontrolwp.com>
2020-10-23 14:05:23 +0100

    add stats summary bar

--- src/lib/src/Integrations/MainWP/Server/UI/PageRender/Sites.php
+++ src/lib/src/Integrations/MainWP/Server/UI/PageRender/Sites.php
@@ -14,31 +14,43 @@ class Sites extends BaseRender {
 		$WP = Services::WpGeneral();
 		$req = Services::Request();
 
+		$statsHead = [
+			'active'       => 0,
+			'inactive'     => 0,
+			'with_issues'  => 0,
+			'needs_update' => 0,
+		];
 		$sites = apply_filters( 'mainwp_getsites', $mwp->child_file, $mwp->child_key );
 		foreach ( $sites as &$site ) {
 			$sync = $this->getSiteShieldSyncInfo( $site );
 			$site[ 'shield' ] = $sync->getRawDataAsArray();
 			$site[ 'shield' ][ 'is_installed' ] = $sync->installed_at ?? false;
 			if ( $sync->installed_at > 0 ) {
+				$statsHead[ 'active' ]++;
 				$site[ 'shield' ][ 'sync_at_text' ] = $WP->getTimeStringForDisplay( $sync->sync_at );
 				$site[ 'shield' ][ 'sync_at_diff' ] = $req->carbon()->setTimestamp( $sync->sync_at )->diffForHumans();
+
+				$statsHead[ 'with_issues' ] += $sync->has_update ? 1 : 0;
+				$statsHead[ 'needs_update' ] += $sync->has_update ? 1 : 0;
+			}
+			else {
+				$statsHead[ 'inactive' ]++;
 			}
 		}
+
 		$data = [
-			'vars' => [
-				'sites' => $sites,
+			'vars'    => [
+				'sites'      => $sites,
+				'stats_head' => $statsHead,
+			],
+			'strings' => [
+				'active'       => __( 'Active', 'wp-simple-firewall' ),
+				'inactive'     => __( 'Inactive', 'wp-simple-firewall' ),
+				'with_issues'  => __( 'With Issues', 'wp-simple-firewall' ),
+				'needs_update' => __( 'Needs Update', 'wp-simple-firewall' ),
 			]
 		];
 
-//		$sites = apply_filters( 'mainwp_getsites', $con->getRootFile(), $this->childKey );
-//		var_dump( $sites );
-//		?>
-		<!--		https://mainwp.com/passing-information-to-your-child-sites/-->
-		<!--		<div id="uploader_select_sites_box" class="mainwp_config_box_right">-->
-		<!--        --><?php
-//		do_action( 'mainwp_select_sites_box', __( "Select Sites", 'mainwp' ), 'checkbox', true, true, 'mainwp_select_sites_box_right', "", [], [] );
-//		?><!--</div>-->
-		<!--		--><?php
 		return $data;
 	}
 
