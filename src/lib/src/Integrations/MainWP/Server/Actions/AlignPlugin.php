4a16d695af9b18790e49df8c60e7e707c23c640a
Paul G <paul@icontrolwp.com>
2020-10-29 17:31:34 +0000

    add plugin install for align plugin

--- src/lib/src/Integrations/MainWP/Server/Actions/AlignPlugin.php
+++ src/lib/src/Integrations/MainWP/Server/Actions/AlignPlugin.php
@@ -5,6 +5,7 @@ namespace FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Server\Act
 use FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Common\Consumers\MWPSiteConsumer;
 use FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Server;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\PluginControllerConsumer;
+use FernleafSystems\Wordpress\Services\Utilities\WpOrg\Plugin\Api;
 use MainWP\Dashboard\MainWP_Connect;
 use MainWP\Dashboard\MainWP_Sync;
 
@@ -14,11 +15,9 @@ class AlignPlugin {
 	use MWPSiteConsumer;
 
 	public function run() {
-		$site = $this->getMwpSite();
-
 		$oStatus = ( new Server\Data\PluginStatus() )
 			->setCon( $this->getCon() )
-			->setMwpSite( $site );
+			->setMwpSite( $this->getMwpSite() );
 
 		switch ( $oStatus->status() ) {
 
@@ -38,6 +37,11 @@ class AlignPlugin {
 				$this->sync();
 				break;
 
+			case Server\Data\PluginStatus::NOT_INSTALLED:
+				$this->install();
+				$this->sync();
+				break;
+
 			case Server\Data\PluginStatus::ACTIVE:
 			default:
 				// nothing
@@ -47,7 +51,7 @@ class AlignPlugin {
 
 	public function update() :bool {
 		$siteObj = $this->getMwpSite()->siteobj;
-		$info = MainWP_Connect::fetch_url_authed(
+		MainWP_Connect::fetch_url_authed(
 			$siteObj,
 			'upgradeplugintheme',
 			[
@@ -59,6 +63,29 @@ class AlignPlugin {
 			],
 			true
 		);
+		return true;
+	}
+
+	public function install() :bool {
+		$siteObj = [ $this->getMwpSite()->siteobj ];
+		$urlInstall = ( new Api() )
+			->setWorkingSlug( 'wp-simple-firewall' )
+			->getInfo()->download_link;
+		$info = MainWP_Connect::fetch_urls_authed(
+			$siteObj,
+			'installplugintheme',
+			[
+				'type'           => 'plugin',
+				'url'            => $urlInstall,
+				'activatePlugin' => 'yes',
+				'overwrite'      => 'yes',
+			],
+			null,
+			$o
+		);
+		var_dump( $o );
+		var_dump( $info );
+		die();
 		error_log( var_export( $info, true ) );
 		return true;
 	}
@@ -81,6 +108,7 @@ class AlignPlugin {
 			]
 		);
 
-		return $info[ 'status' ] ?? false === 'SUCCESS';
+		$status = $info[ 'status' ] ?? false;
+		return $status === 'SUCCESS';
 	}
 }
