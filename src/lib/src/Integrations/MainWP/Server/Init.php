d9bc9257780ee6d30645d3e0b6b04e736382e8ea
Paulie G <paul@icontrolwp.com>
2020-10-30 11:45:39 +0000

    prevent full loading of the mainwp server-side extension

--- src/lib/src/Integrations/MainWP/Server/Init.php
+++ src/lib/src/Integrations/MainWP/Server/Init.php
@@ -5,6 +5,7 @@ namespace FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Server;
 use FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Controller;
 use FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Server\Data\SyncHandler;
 use FernleafSystems\Wordpress\Plugin\Shield\Integrations\MainWP\Server\UI\ExtensionSettingsPage;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\Plugin\Options;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\PluginControllerConsumer;
 
 class Init {
@@ -16,6 +17,15 @@ class Init {
 	 * @throws \Exception
 	 */
 	public function run() :string {
+		$con = $this->getCon();
+		/** @var Options $pluginOpts */
+		$pluginOpts = $con->getModule_Plugin()->getOptions();
+
+		// TODO: Consider have an "error" screen message to show it's not enabled instead?
+		if ( !$pluginOpts->isEnabledMainWP() ) {
+			throw new \Exception( 'MainWP Extension is not enabled' );
+		}
+
 		add_filter( 'mainwp_getextensions', function ( $exts ) {
 			$con = $this->getCon();
 			$exts[] = [
@@ -27,13 +37,13 @@ class Init {
 			return $exts;
 		}, 10, 1 );
 
-		$childEnabled = apply_filters( 'mainwp_extension_enabled_check', $this->getCon()->getRootFile() );
+		$childEnabled = apply_filters( 'mainwp_extension_enabled_check', $con->getRootFile() );
 		$key = $childEnabled[ 'key' ] ?? '';
 		if ( empty( $key ) ) {
 			throw new \Exception( 'No child key provided' );
 		}
 
-		if ( Controller::isMainWPServerVersionSupported() && $this->getCon()->isPremiumActive() ) {
+		if ( Controller::isMainWPServerVersionSupported() && $con->isPremiumActive() ) {
 			( new SyncHandler() )
 				->setCon( $this->getCon() )
 				->execute();
