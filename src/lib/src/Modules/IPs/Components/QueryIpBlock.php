ec4fe01874acbc531b985c5c0b2e6e9f704acc9a
Paulie G <paul@icontrolwp.com>
2021-02-02 10:24:34 +0000

    fix: redirect to home page after user auto unblock

--- src/lib/src/Modules/IPs/Components/QueryIpBlock.php
+++ src/lib/src/Modules/IPs/Components/QueryIpBlock.php
@@ -16,29 +16,21 @@ class QueryIpBlock {
 	use Shield\Modules\ModConsumer;
 	use IpAddressConsumer;
 
-	/**
-	 * @var string
-	 */
-	private $sIP;
-
-	/**
-	 * @return bool - true if IP is blocked, false otherwise
-	 */
-	public function run() {
-		$bIpBlocked = false;
+	public function run() :bool {
+		$isBlocked = false;
 
 		$oIP = $this->getBlockedIpRecord();
 		if ( $oIP instanceof Databases\IPs\EntryVO ) {
 
-			$bIpBlocked = true;
+			$isBlocked = true;
 
 			/** @var IPs\ModCon $mod */
 			$mod = $this->getMod();
-			/** @var Databases\IPs\Update $oUp */
-			$oUp = $mod->getDbHandler_IPs()->getQueryUpdater();
-			$oUp->updateLastAccessAt( $oIP );
+			/** @var Databases\IPs\Update $upd */
+			$upd = $mod->getDbHandler_IPs()->getQueryUpdater();
+			$upd->updateLastAccessAt( $oIP );
 		}
-		return $bIpBlocked;
+		return $isBlocked;
 	}
 
 	/**
