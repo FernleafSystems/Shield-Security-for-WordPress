6d67cc82cd0cdf8f7da7558058afb3176650872a
Paul G <paul@icontrolwp.com>
2021-02-04 16:27:25 +0000

    update build

--- plugin-spec.php
+++ plugin-spec.php
@@ -2,7 +2,7 @@
   "properties":       {
     "version":                 "10.1.6",
     "release_timestamp":       1611830119,
-    "build":                   "202101.2901",
+    "build":                   "202102.0401",
     "slug_parent":             "icwp",
     "slug_plugin":             "wpsf",
     "human_name":              "Shield Security",
