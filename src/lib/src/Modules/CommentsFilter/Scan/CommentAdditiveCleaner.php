0de6921155074533f7852a59693f07698512da82
Paulie G <paul@icontrolwp.com>
2021-03-10 16:13:59 +0000

    automatically remove comment spam text upon approval/pending/unspamming

---
+++ src/lib/src/Modules/CommentsFilter/Scan/CommentAdditiveCleaner.php
@@ -0,0 +1,26 @@
+<?php declare( strict_types=1 );
+
+namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\CommentsFilter\Scan;
+
+use FernleafSystems\Utilities\Logic\ExecOnce;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\ModConsumer;
+use FernleafSystems\Wordpress\Plugin\Shield\Utilities;
+
+class CommentAdditiveCleaner {
+
+	use ModConsumer;
+	use ExecOnce;
+
+	protected function run() {
+		add_action( 'wp_set_comment_status', function ( $commentID, $newStatus ) {
+			if ( in_array( $newStatus, [ '0', 'hold', '1', 'approve' ], true ) ) {
+				wp_update_comment(
+					[
+						'comment_ID'      => $commentID,
+						'comment_content' => preg_replace( '/## Comment SPAM Protection:.*\s##/m', '', get_comment( $commentID )->comment_content ),
+					]
+				);
+			}
+		}, 10, 2 );
+	}
+}
