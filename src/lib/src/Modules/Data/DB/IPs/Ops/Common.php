f0f286153c7383b091835959c43b7d61aaadbe44
Paulie G <paul@icontrolwp.com>
2021-09-16 11:18:15 +0100

    control and handling of logs and requests where the IP address isn't available.

--- src/lib/src/Modules/Data/DB/IPs/Ops/Common.php
+++ src/lib/src/Modules/Data/DB/IPs/Ops/Common.php
@@ -2,9 +2,9 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Data\DB\IPs\Ops;
 
+use FernleafSystems\Wordpress\Plugin\Core\Databases\Base\Traits\FilterByIP;
+
 trait Common {
 
-	public function filterByIPHuman( string $ip ) :self {
-		return $this->addRawWhere( [ '`ip`', '=', sprintf( "INET6_ATON('%s')", $ip ) ] );
-	}
+	use FilterByIP;
 }
