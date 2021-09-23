900c033505eacdabf7186e9ea6d2042d5cc20e99
Paul G <paul@icontrolwp.com>
2021-09-23 11:14:44 +0100

    mysql requirements handling ignore mariadb

--- src/lib/src/Controller/Controller.php
+++ src/lib/src/Controller/Controller.php
@@ -221,8 +221,11 @@ class Controller extends DynPropertiesClass {
 				$reqsMsg[] = sprintf( 'WordPress does not meet minimum version. Required Version: %s.', $wp );
 			}
 
+			$mysqlInfo = Services::WpDb()->loadWpdb()->db_version();
+			$mysqlV = preg_replace( '/[^0-9.].*/', '', $mysqlInfo );
 			$mysql = $this->cfg->requirements[ 'mysql' ];
-			if ( !empty( $mysql ) && version_compare( Services::WpDb()->loadWpdb()->db_version(), $mysql, '<' ) ) {
+			if ( !empty( $mysql ) && version_compare( $mysqlV, $mysql, '<' )
+				 && ( stripos( $mysqlInfo, 'MariaDB' ) === false ) ) {
 				$reqsMsg[] = sprintf( "Your MySQL database server doesn't support IPv6 addresses. Your Version: %s; Required MySQL Version: %s;",
 					Services::WpDb()->loadWpdb()->db_version(), $mysql );
 			}
