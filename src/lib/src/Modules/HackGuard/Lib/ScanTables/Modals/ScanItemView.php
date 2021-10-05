7edb56b00e1c758917657bb27b9d0c6f2ab4c939
Paul G <paul@icontrolwp.com>
2021-10-05 14:20:19 +0100

    improve the code diff view for scan items

--- src/lib/src/Modules/HackGuard/Lib/ScanTables/Modals/ScanItemView.php
+++ src/lib/src/Modules/HackGuard/Lib/ScanTables/Modals/ScanItemView.php
@@ -2,14 +2,14 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\Lib\ScanTables\Modals;
 
-use FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\Lib\Hashes\Query;
+use FernleafSystems\Wordpress\Services\Utilities\File\ConvertLineEndings;
+use FernleafSystems\Wordpress\Services\Utilities\WpOrg;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\ModCon;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\Scan\Results\ResultsRetrieve;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\ModConsumer;
 use FernleafSystems\Wordpress\Plugin\Shield\Scans;
 use FernleafSystems\Wordpress\Services\Services;
 use FernleafSystems\Wordpress\Services\Utilities\Integrations\WpHashes\Util\Diff;
-use FernleafSystems\Wordpress\Services\Utilities\WpOrg\Wp\Files;
 
 class ScanItemView {
 
@@ -34,6 +34,15 @@ class ScanItemView {
 			throw new \Exception( 'Non-file scan items are not supported yet.' );
 		}
 
+		try {
+			$diff = $this->getFileDiff( $item->path_full );
+			$hasDiff = true;
+		}
+		catch ( \Exception $e ) {
+			$diff = '';
+			$hasDiff = false;
+		}
+
 		return [
 			'path'     => \esc_html( $item->path_fragment ),
 			'contents' => $mod->renderTemplate(
@@ -43,8 +52,11 @@ class ScanItemView {
 						'tab_filecontents' => ( new FileContents() )
 												  ->setMod( $mod )
 												  ->run( $rid )[ 'contents' ],
-						'tab_diff'         => $this->getFileDiff( $item->path_full ),
+						'tab_diff'         => $diff,
 						'tab_history'      => 'HISTORY',
+					],
+					'flags'   => [
+						'has_diff' => $hasDiff
 					],
 					'strings' => [
 						'tab_filecontents' => 'Contents',
@@ -56,38 +68,53 @@ class ScanItemView {
 		];
 	}
 
+	/**
+	 * @throws \Exception
+	 */
 	private function getFileDiff( string $pathFull ) :string {
 		$original = '';
 		$coreHashes = Services::CoreFileHashes();
 		if ( $coreHashes->isCoreFile( $pathFull ) ) {
-			// TODO: Convoluted - get the content directly
 			$original = Services::WpFs()->getFileContent(
-				( new Files() )->getOriginalFileFromVcs( $pathFull )
+				( new WpOrg\Wp\Files() )->getOriginalFileFromVcs( $pathFull )
 			);
 		}
 		else {
-			// TODO for plugins/themes
-			$exists = ( new Query() )
-				->setMod( $this->getMod() )
-				->fileExistsInHash( $pathFull );
+			$plugin = ( new WpOrg\Plugin\Files() )->findPluginFromFile( $pathFull );
+			if ( !empty( $plugin ) && $plugin->isWpOrg() && $plugin->svn_uses_tags ) {
+				$original = Services::WpFs()->getFileContent(
+					( new WpOrg\Plugin\Files() )->getOriginalFileFromVcs( $pathFull )
+				);
+			}
+			else {
+				$theme = ( new WpOrg\Theme\Files() )->findThemeFromFile( $pathFull );
+				if ( !empty( $theme ) && $theme->isWpOrg() ) {
+					$original = Services::WpFs()->getFileContent(
+						( new WpOrg\Theme\Files() )->getOriginalFileFromVcs( $pathFull )
+					);
+				}
+			}
 		}
 
 		if ( empty( $original ) ) {
-			$diff = 'no diff';
-		}
-		else {
-			$res = ( new Diff() )->getDiff( $original, Services::WpFs()->getFileContent( $pathFull ) );
-			if ( !is_array( $res ) || empty( $res[ 'html' ] ) ) {
-				throw new \Exception( 'Could not get a valid diff for this file.' );
-			}
-			$diff = sprintf( '<style>%s</style>%s',
-				'table.diff.diff-wrapper tbody tr td:nth-child(2){ width:auto;}'.
-				'table.diff.diff-wrapper { table-layout: auto;}'.
-				base64_decode( $res[ 'html' ][ 'css_default' ] ),
-				base64_decode( $res[ 'html' ][ 'content' ] )
-			);
+			throw new \Exception( 'No original file available to diff' );
 		}
 
-		return $diff;
+		$converter = new ConvertLineEndings();
+		$res = ( new Diff() )->getDiff(
+			$converter->dosToLinux( $original ),
+			$converter->fileDosToLinux( $pathFull )
+		);
+
+		if ( !is_array( $res ) || empty( $res[ 'html' ] ) ) {
+			throw new \Exception( 'Could not get a valid diff for this file.' );
+		}
+
+		return sprintf( '<style>%s</style>%s',
+			'table.diff.diff-wrapper tbody tr td:nth-child(2){ width:auto;}'.
+			'table.diff.diff-wrapper { table-layout: auto;}'.
+			base64_decode( $res[ 'html' ][ 'css_default' ] ),
+			base64_decode( $res[ 'html' ][ 'content' ] )
+		);
 	}
 }
