ea0f6e84a056293f2635e43e2757c2876616335c
Paulie G <paul@icontrolwp.com>
2022-03-01 10:23:21 +0000

    allow request logger to take on custom handlers

--- src/lib/src/Modules/Traffic/Lib/RequestLogger.php
+++ src/lib/src/Modules/Traffic/Lib/RequestLogger.php
@@ -1,10 +1,10 @@
-<?php
+<?php declare( strict_types=1 );
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Traffic\Lib;
 
 use FernleafSystems\Wordpress\Plugin\Shield\Logging\Processors;
-use FernleafSystems\Wordpress\Plugin\Shield\Modules\Traffic;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Base\Common\ExecOnceModConsumer;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\Traffic;
 use FernleafSystems\Wordpress\Services\Services;
 use FernleafSystems\Wordpress\Services\Utilities\Net\IpID;
 use Monolog\Logger;
@@ -20,6 +20,8 @@ class RequestLogger extends ExecOnceModConsumer {
 		$this->getLogger()
 			 ->pushHandler( ( new LogHandlers\LocalDbWriter() )->setMod( $this->getMod() ) );
 
+		$this->pushCustomHandlers();
+
 		add_action( $this->getCon()->prefix( 'plugin_shutdown' ), function () {
 			if ( $this->isRequestToBeLogged() ) {
 				$this->getLogger()->log( 'debug', 'log request' );
@@ -27,13 +29,21 @@ class RequestLogger extends ExecOnceModConsumer {
 		}, 1000 ); // high enough to come after audit trail
 	}
 
+	private function pushCustomHandlers() {
+		$custom = apply_filters( 'shield/custom_request_log_handlers', [] );
+		array_map(
+			function ( $handler ) {
+				$this->getLogger()->pushHandler( $handler );
+			},
+			( $this->getCon()->isPremiumActive() && is_array( $custom ) ) ? $custom : []
+		);
+	}
+
 	private function isRequestToBeLogged() :bool {
 		/** @var Traffic\Options $opts */
 		$opts = $this->getOptions();
 		return !$this->getCon()->plugin_deleting
-			   && apply_filters( 'shield/is_log_traffic',
-				$opts->isTrafficLoggerEnabled() && !$this->isCustomExcluded() && !$this->isRequestTypeExcluded()
-			   );
+			   && apply_filters( 'shield/is_log_traffic', $opts->isTrafficLoggerEnabled() && !$this->isCustomExcluded() && !$this->isRequestTypeExcluded() );
 	}
 
 	public function getLogger() :Logger {
@@ -49,6 +59,8 @@ class RequestLogger extends ExecOnceModConsumer {
 	}
 
 	private function isRequestTypeExcluded() :bool {
+		$srvProviders = Services::ServiceProviders();
+		$ipIdentity = Services::IP()->getIpDetector()->getIPIdentity();
 		/** @var Traffic\Options $opts */
 		$opts = $this->getOptions();
 		$excl = $opts->getReqTypeExclusions();
@@ -58,11 +70,11 @@ class RequestLogger extends ExecOnceModConsumer {
 				   || ( in_array( 'logged_in', $excl ) && $isLoggedIn )
 				   || ( in_array( 'ajax', $excl ) && Services::WpGeneral()->isAjax() )
 				   || ( in_array( 'cron', $excl ) && Services::WpGeneral()->isCron() )
-				   || ( in_array( 'server', $excl ) && $this->isThisServer() );
+				   || ( in_array( 'server', $excl ) && $ipIdentity === IpID::THIS_SERVER );
 
 		if ( !$exclude && !$isLoggedIn ) {
-			$exclude = ( in_array( 'search', $excl ) && $this->isServiceIp_Search() )
-					   || ( in_array( 'uptime', $excl ) && $this->isServiceIp_Uptime() );
+			$exclude = ( in_array( 'search', $excl ) && in_array( $ipIdentity, $srvProviders->getSearchProviders() ) )
+					   || ( in_array( 'uptime', $excl ) && in_array( $ipIdentity, $srvProviders->getUptimeProviders() ) );
 		}
 
 		return $exclude;
@@ -85,16 +97,25 @@ class RequestLogger extends ExecOnceModConsumer {
 		return $exclude;
 	}
 
+	/**
+	 * @deprecated 14.1
+	 */
 	private function isServiceIp_Search() :bool {
 		return in_array( Services::IP()->getIpDetector()->getIPIdentity(),
 			Services::ServiceProviders()->getSearchProviders() );
 	}
 
+	/**
+	 * @deprecated 14.1
+	 */
 	private function isServiceIp_Uptime() :bool {
-		return in_array( Services::IP()->getIpDetector()->getIPIdentity(),
-			Services::ServiceProviders()->getUptimeProviders() );
+		$IP = Services::IP();
+		return in_array( $IP->getIpDetector()->getIPIdentity(), Services::ServiceProviders()->getUptimeProviders() );
 	}
 
+	/**
+	 * @deprecated 14.1
+	 */
 	private function isThisServer() :bool {
 		return Services::IP()->getIpDetector()->getIPIdentity() === IpID::THIS_SERVER;
 	}
