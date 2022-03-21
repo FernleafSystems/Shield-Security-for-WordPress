6386dad6ff5a0456e7b68e989218e12f84a6c53b
Paul G <paul@icontrolwp.com>
2022-03-21 11:49:03 +0000

    add a much more robust database upgrader

---
+++ src/lib/src/Databases/ReqLogs/QueueReqDbRecordMigrator.php
@@ -0,0 +1,101 @@
+<?php declare( strict_types=1 );
+
+namespace FernleafSystems\Wordpress\Plugin\Shield\Databases\ReqLogs;
+
+use FernleafSystems\Wordpress\Plugin\Shield;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\Data\DB\ReqLogs\Ops as ReqDB;
+
+class QueueReqDbRecordMigrator extends Shield\Databases\Utility\QueueDbRecordsMigrator {
+
+	public function __construct() {
+		parent::__construct( 'db_upgrader_reqlogs' );
+	}
+
+	/**
+	 * @return ReqDB\Select
+	 */
+	protected function getDbSelector() {
+		/** @var Shield\Modules\Data\ModCon $mod */
+		$mod = $this->getMod();
+		/** @var ReqDB\Select $select */
+		$select = $mod->getDbH_ReqLogs()->getQuerySelector();
+		return $select->addWhere( 'type', '' );
+	}
+
+	protected function processRecord( $record ) {
+		/** @var Shield\Modules\Data\DB\ReqLogs\Ops\Record */
+		/** @var Shield\Modules\Data\ModCon $mod */
+		$mod = $this->getMod();
+
+		$upgradeData = [
+			'type' => ReqDB\Handler::TYPE_HTTP,
+			'path' => $record->path,
+		];
+
+		$meta = $record->meta;
+
+		if ( $meta[ 'ua' ] === 'wpcli' ) {
+			$isWpCli = true;
+			$upgradeData[ 'type' ] = ReqDB\Handler::TYPE_WPCLI;
+			unset( $meta[ 'ua' ] );
+		}
+		else {
+			$isWpCli = false;
+		}
+
+		if ( isset( $meta[ 'code' ] ) && is_numeric( $meta[ 'code' ] ) ) {
+			$upgradeData[ 'code' ] = (int)$meta[ 'code' ];
+			unset( $meta[ 'code' ] );
+		}
+
+		if ( isset( $meta[ 'offense' ] ) ) {
+			$upgradeData[ 'offense' ] = true;
+			unset( $meta[ 'offense' ] );
+		}
+
+		if ( !empty( $meta[ 'path' ] ) ) {
+			$parts = explode( $isWpCli ? ' ' : '?', (string)$meta[ 'path' ], 2 );
+			$upgradeData[ 'path' ] = $parts[ 0 ];
+			if ( !empty( $parts[ 1 ] ) ) {
+				$meta[ 'query' ] = $parts[ 1 ];
+			}
+			unset( $meta[ 'path' ] );
+		}
+
+		if ( !empty( $meta[ 'verb' ] ) ) {
+			$upgradeData[ 'verb' ] = strtoupper( (string)$meta[ 'verb' ] );
+			unset( $meta[ 'verb' ] );
+		}
+
+		if ( !empty( $meta[ 'uid' ] ) ) {
+			$upgradeData[ 'uid' ] = (int)$meta[ 'uid' ];
+			unset( $meta[ 'uid' ] );
+		}
+
+		if ( ( $meta[ 'ua' ] ?? '' ) === 'wpcli' ) {
+			$upgradeData[ 'type' ] = 'WPCLI';
+			unset( $meta[ 'ua' ] );
+		}
+		elseif ( wp_parse_url( admin_url( 'admin-ajax.php' ), PHP_URL_PATH ) === $upgradeData[ 'path' ] ) {
+			$upgradeData[ 'type' ] = ReqDB\Handler::TYPE_AJAX;
+		}
+		elseif ( wp_parse_url( home_url( 'wp-cron.php' ), PHP_URL_PATH ) === $upgradeData[ 'path' ] ) {
+			$upgradeData[ 'type' ] = ReqDB\Handler::TYPE_CRON;
+		}
+		elseif ( wp_parse_url( home_url( 'xmlrpc.php' ), PHP_URL_PATH ) === $upgradeData[ 'path' ] ) {
+			$upgradeData[ 'type' ] = ReqDB\Handler::TYPE_XMLRPC;
+		}
+
+		$record->meta = $meta;
+		$upgradeData[ 'meta' ] = $record->getRawData()[ 'meta' ];
+
+		$success = $mod->getDbH_ReqLogs()
+					   ->getQueryUpdater()
+					   ->updateById( $record->id, $upgradeData );
+		if ( !$success ) {
+			throw new \Exception( 'failed to update' );
+		}
+
+		return $record;
+	}
+}
---
+++ src/lib/src/Databases/Utility/QueueDbRecordsMigrator.php
@@ -0,0 +1,110 @@
+<?php declare( strict_types=1 );
+
+namespace FernleafSystems\Wordpress\Plugin\Shield\Databases\Utility;
+
+use FernleafSystems\Wordpress\Plugin\Core\Databases\Base\Record;
+use FernleafSystems\Wordpress\Plugin\Core\Databases\Base\Select;
+use FernleafSystems\Wordpress\Plugin\Shield;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\ModConsumer;
+use FernleafSystems\Wordpress\Services\Utilities;
+
+abstract class QueueDbRecordsMigrator extends Utilities\BackgroundProcessing\BackgroundProcess {
+
+	use ModConsumer;
+
+	const PAGE_SIZE = 100;
+
+	/**
+	 * Get batch
+	 *
+	 * @return \stdClass Return the first batch from the queue
+	 */
+	protected function get_batch() {
+		$batch = new \stdClass();
+
+		try {
+			$batch->key = rand();
+			$batch->data = $this->getNextItems();
+		}
+		catch ( \Exception $e ) {
+			// This should never happen as "is_queue_empty()" is called before
+			error_log( $e->getMessage() );
+		}
+
+		return $batch;
+	}
+
+	protected function getNextItems() :array {
+		$result = $this->getDbSelector()
+					   ->setLimit( static::PAGE_SIZE )
+					   ->setPage( 1 )
+					   ->queryWithResult();
+		return is_array( $result ) ? $result : [];
+	}
+
+	/**
+	 * @return Select|mixed
+	 */
+	abstract protected function getDbSelector();
+
+	/**
+	 * Task
+	 *
+	 * Override this method to perform any actions required on each
+	 * queue item. Return the modified item for further processing
+	 * in the next pass through. Or, return false to remove the
+	 * item from the queue.
+	 *
+	 * @param Record $item Array of records to be processed.
+	 * @return false
+	 */
+	protected function task( $item ) {
+		try {
+			$this->processRecord( $item );
+		}
+		catch ( \Exception $e ) {
+			$this->cancel_process();
+		}
+		return false;
+	}
+
+	/**
+	 * @param Record $record
+	 * @return Record
+	 * @throws \Exception
+	 */
+	abstract protected function processRecord( $record );
+
+	/**
+	 * Delete queue
+	 *
+	 * @param string $key Key.
+	 * @return $this
+	 */
+	public function delete( $key ) {
+		return $this;
+	}
+
+	/**
+	 * Is queue empty
+	 *
+	 * @return bool
+	 */
+	protected function is_queue_empty() {
+		return $this->getDbSelector()->count() === 0;
+	}
+
+	/**
+	 * Save queue
+	 *
+	 * @return $this
+	 */
+	public function save() {
+		return $this;
+	}
+
+	public function update( $key, $data ) {
+		// Do nothing. Results are stored separately.
+		return $this;
+	}
+}
