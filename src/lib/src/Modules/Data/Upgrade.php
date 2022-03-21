531cb591d4bf942e4afd3cf346f9d2d3788d43a1
Paul G <paul@icontrolwp.com>
2022-03-21 13:26:10 +0000

    upgrade the path length to be 512 long

--- src/lib/src/Modules/Data/Upgrade.php
+++ src/lib/src/Modules/Data/Upgrade.php
@@ -3,13 +3,18 @@
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Data;
 
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Base;
-use FernleafSystems\Wordpress\Plugin\Shield\Modules\Data\Lib\UpgradeReqLogsTable;
+use FernleafSystems\Wordpress\Services\Services;
 
 class Upgrade extends Base\Upgrade {
 
-	protected function upgrade_1411() {
-		( new UpgradeReqLogsTable() )
-			->setMod( $this->getMod() )
-			->execute();
+	public function upgrade_1414() {
+		/** @var ModCon $mod */
+		$mod = $this->getMod();
+		$schema = $mod->getDbH_ReqLogs()->getTableSchema();
+		$res = Services::WpDb()->doSql( sprintf( 'ALTER TABLE `%s` MODIFY COLUMN %s %s;',
+			$schema->table,
+			'path',
+			$schema->enumerateColumns()[ 'path' ]
+		) );
 	}
 }
