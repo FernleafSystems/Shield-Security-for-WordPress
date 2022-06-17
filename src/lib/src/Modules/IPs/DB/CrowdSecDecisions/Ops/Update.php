f71629fbd4f69fc944e85a2e0039d974dbea893f
Paul G <paul@icontrolwp.com>
2022-06-17 22:00:13 +0100

    blocking a request using Crowdsec will set the last_access_at column

--- src/lib/src/Modules/IPs/DB/CrowdSecDecisions/Ops/Update.php
+++ src/lib/src/Modules/IPs/DB/CrowdSecDecisions/Ops/Update.php
@@ -3,8 +3,14 @@
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\DB\CrowdSecDecisions\Ops;
 
 use FernleafSystems\Wordpress\Plugin\Core\Databases\Base;
+use FernleafSystems\Wordpress\Services\Services;
 
-class Select extends Base\Select {
+class Update extends Base\Update {
 
 	use Common;
+
+	public function updateLastAccessAt() :bool {
+		return $this->setUpdateData( [ 'last_access_at' => Services::Request()->ts() ] )
+					->query();
+	}
 }
--- src/lib/src/Modules/IPs/Lib/BlockRequestCrowdsec.php
+++ src/lib/src/Modules/IPs/Lib/BlockRequestCrowdsec.php
@@ -4,6 +4,12 @@ namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\Lib;
 
 use FernleafSystems\Wordpress\Plugin\Shield\Blocks\RenderBlockPages\RenderBlockIpCrowdSec;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Base\Common\ExecOnceModConsumer;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\DB\CrowdSecDecisions\{
+	CrowdSecRecord,
+	LoadCrowdsecDecisions,
+	Ops as CrowdsecDecisionsDB
+};
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\ModCon;
 
 class BlockRequestCrowdsec extends ExecOnceModConsumer {
 
@@ -12,7 +18,24 @@ class BlockRequestCrowdsec extends ExecOnceModConsumer {
 	}
 
 	protected function run() {
+		/** @var ModCon $mod */
+		$mod = $this->getMod();
+
+		/** @var CrowdSecRecord[] $records */
+		$records = ( new LoadCrowdsecDecisions() )
+			->setMod( $mod )
+			->setIP( $this->getCon()->this_req->ip )
+			->select();
+		if ( count( $records ) === 1 ) {
+			$theRecord = array_shift( $records );
+			/** @var CrowdsecDecisionsDB\Update $updater */
+			$updater = $mod->getDbH_CrowdSecSignals()->getQueryUpdater();
+			$updater->filterByIP( $theRecord->id )
+					->updateLastAccessAt();
+		}
+
 		$this->getCon()->fireEvent( 'conn_kill_crowdsec' );
+
 		( new RenderBlockIpCrowdSec() )
 			->setMod( $this->getMod() )
 			->display();
