e539151faeb1315b9953ab5d19f01a32b48a5b25
Paul G <paul@icontrolwp.com>
2022-12-08 10:51:04 +0000

    refine our simple debug system

--- src/lib/src/Modules/Insights/ActionRouter/Actions/Debug/SimplePluginTests.php
+++ src/lib/src/Modules/Insights/ActionRouter/Actions/Debug/SimplePluginTests.php
@@ -2,10 +2,18 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Insights\ActionRouter\Actions\Debug;
 
+use FernleafSystems\Wordpress\Plugin\Shield\Modules;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Insights\ActionRouter\Actions\BaseAction;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Insights\ActionRouter\Exceptions\ActionException;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\DB\IpRules\LoadIpRules;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\DB\IpRules\Ops\Handler;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\IPs\Lib\Bots\ShieldNET\BuildData;
+use FernleafSystems\Wordpress\Plugin\Shield\Scans\Afs\Processing\FileScanOptimiser;
 use FernleafSystems\Wordpress\Plugin\Shield\ShieldNetApi;
 use FernleafSystems\Wordpress\Plugin\Shield\Tests\RunTests;
+use FernleafSystems\Wordpress\Services\Utilities\File\Search\SearchFile;
+use FernleafSystems\Wordpress\Services\Utilities\Integrations\WpHashes\Verify\Email;
+use FernleafSystems\Wordpress\Services\Utilities\Net\IpID;
 
 class SimplePluginTests extends BaseAction {
 
@@ -19,6 +27,13 @@ class SimplePluginTests extends BaseAction {
 		ob_start();
 		$this->{$testMethod}();
 		$this->response()->action_response_data = [ ob_get_clean() ];
+	}
+
+	private function dbg_reporting() {
+		$this->getCon()
+			 ->getModule_Reporting()
+			 ->getReportingController()
+			 ->runHourlyCron();
 	}
 
 	private function handshake() {
@@ -35,5 +50,196 @@ class SimplePluginTests extends BaseAction {
 			->run();
 		$output = ob_get_clean();
 		return [ print_r( $output, true ) ];
+	}
+
+	private function cleanIPs() {
+
+		( new Modules\IPs\DB\IpRules\CleanIpRules() )
+			->setMod( $this->getCon()->getModule_IPs() )
+			->expired_Crowdsec();
+		return;
+
+		$ip = '150.95.219.153';
+		$mod = $this->getCon()->getModule_IPs();
+
+		$rule = ( new Modules\IPs\Lib\IpRules\IpRuleStatus( $ip ) )
+			->setMod( $mod )
+			->getRuleForAutoBlock();
+		var_dump( $rule );
+		die();
+
+		try {
+			( new Modules\IPs\DB\IpRules\MergeAutoBlockRules() )
+				->setMod( $mod )
+				->byIP( $ip );
+		}
+		catch ( \Exception $e ) {
+			var_dump( $e->getMessage() );
+		}
+		die();
+
+		$rule = ( new Modules\IPs\Lib\IpRules\IpRuleStatus() )
+			->setMod( $mod )
+			->getRuleForAutoBlock();
+		var_dump( $rule );
+		( new Modules\IPs\DB\IpRules\CleanIpRules() )
+			->setMod( $this->getCon()->getModule_IPs() )
+			->duplicates_AutoBlock();
+//		( new Modules\IPs\DB\IpRules\CleanIpRules() )
+//			->setMod( $this->getCon()->getModule_IPs() )
+//			->execute();
+//		( new Modules\Data\Lib\CleanDatabases() )
+//			->setMod( $this->getCon()->getModule_Data() )
+//			->execute();
+	}
+
+	private function pagedIpRules() {
+		$modIPs = $this->getCon()->getModule_IPs();
+
+		$loader = ( new LoadIpRules() )->setMod( $modIPs );
+		$loader->wheres = [
+			sprintf( "`ir`.`type`='%s'", Handler::T_CROWDSEC )
+		];
+		$loader->ip_table_select_fields = [
+			'INET6_NTOA(`ips`.`ip`) as `ip`',
+		];
+		$loader->joined_table_select_fields = [
+			'ip_ref',
+			'cidr',
+		];
+		$loader->limit = 10;
+
+		$page = 0;
+		do {
+			$loader->offset = $page*$loader->limit;
+			$records = $loader->select();
+			var_dump( $loader->select() );
+			$page++;
+		} while ( !empty( $records ) );
+	}
+
+	private function iputil() {
+		$address = \IPLib\Factory::parseAddressString( '90.250.11.231' );
+		var_dump( $address->getRangeType() );
+		$address = \IPLib\Factory::parseAddressString( '10.0.0.1' );
+		var_dump( $address->getRangeType() );
+		$address = \IPLib\Factory::parseAddressString( '127.0.0.1' );
+		var_dump( $address->getRangeType() );
+
+		$address = \IPLib\Factory::parseRangeString( '90.250.11.21' );
+		var_dump( $address->asSubnet()->toString() );
+		$address = \IPLib\Factory::parseRangeString( '90.250.11.21/32' );
+		var_dump( $address->asSubnet()->toString() );
+		$address = \IPLib\Factory::parseRangeString( '90.250.11.21/24' );
+		var_dump( $address->asPattern()->toString() );
+		$address = \IPLib\Factory::parseRangeString( '90.250.11.21/27' );
+		var_dump( $address->asSubnet()->toString() );
+		$address = \IPLib\Factory::parseRangeString( '90.250.11.21/10' );
+		var_dump( $address->asSubnet()->toString() );
+	}
+
+	private function crowdsec() {
+		$modIPs = $this->getCon()->getModule_IPs();
+		$csCon = $modIPs->getCrowdSecCon();
+		$API = $csCon->getApi();
+
+//		$auth = $API->getCsAuth();
+//		var_dump($auth);
+
+		try {
+//			$res = $this->getCon()
+//						->getModule_License()
+//						->getLicenseHandler()
+//						->getLicense()->crowdsec[ 'scenarios' ] ?? [];
+
+			error_log( 'memory: '.round( memory_get_usage()/1024/1024 ) );
+			var_dump( 'api ready: '.var_export( $API->isReady(), true ) );
+//			$res = ( new DecisionsDownload( $api->getAuthorizationToken(), 'crowdsec/1.2.1' ) )->run();
+
+//			var_dump( $modIPs->getOptions()->getOpt('crowdsec_cfg') );
+//			var_dump( $csCon->getApi()->getAuthStatus() );
+//			var_dump( $csCon->getApi()->getAuthorizationToken() );
+//			$csCon->getApi()->machineEnroll( false );
+//			var_dump( $csCon->getApi()->getAuthStatus() );
+//			var_dump( $modIPs->getCrowdSecCon()->cfg );
+//			var_dump( $csCon->getApi()->getAuthorizationToken() );
+//			( new Modules\IPs\Lib\CrowdSec\Signals\PushSignalsToCS() )
+//				->setMod( $this->getCon()->getModule_IPs() )
+//				->execute();
+			( new Modules\IPs\Lib\CrowdSec\Decisions\ImportDecisions() )
+				->setMod( $modIPs )
+				->runImport();
+//			var_dump( $d );
+//			$res = ( new Modules\IPs\Lib\CrowdSec\Api\DecisionsDownload(
+//				$csCon->getApi()->getAuthorizationToken(),
+//				$csCon->getApi()->getApiUserAgent()
+//			) )->run();
+//			$res = ( new RetrieveScenarios() )
+//				->setMod( $this->getMod() )
+//				->retrieve();
+//			$res = ( new DecisionsDownload( $csCon->getApi()->getAuthorizationToken() ) )->run();
+		}
+		catch ( \Exception $e ) {
+			var_dump( $e->getMessage() );
+		}
+		var_dump( $res ?? 'unset' );
+		var_dump( 'ready: '.var_export( $API->isReady(), true ) );
+	}
+
+	private function cleanoptimisehashes() {
+		( new FileScanOptimiser() )
+			->setMod( $this->getMod() )
+			->cleanStaleHashesOlderThan( 1654091228 );
+	}
+
+	private function testFileSearch() {
+		$searcher = new SearchFile( $this->getCon()->root_file );
+		var_dump( $searcher->exists( 'a' ) );
+		var_dump( $searcher->multipleFindFirst( [ 'init', 'if' ] ) );
+	}
+
+	private function testAAAA( string $hostname ) {
+		$id = ( new IpID( '2a03:2880:32ff:11::face:b00c', 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)' ) )->run();
+		var_dump( $id );
+//		$record = dns_get_record( $hostname, DNS_AAAA );
+//		var_dump( $record );
+	}
+
+	private function checkIP( string $ip ) {
+		$id = ( new IpID( $ip, 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' ) )->run();
+		var_dump( $id );
+	}
+
+	private function checkEmail( string $email ) {
+		$apiToken = $this->getCon()
+						 ->getModule_License()
+						 ->getWpHashesTokenManager()
+						 ->getToken();
+		if ( !empty( $apiToken ) ) {
+			$verifys = ( new Email( $apiToken ) )->getEmailVerification( $email );
+			var_dump( $verifys );
+		}
+	}
+
+	private function dbIntegrity() {
+		( new Lib\Ops\VerifyDatabaseIntegrity() )
+			->setCon( $this->getCon() )
+			->run();
+	}
+
+	private function testbotsignals() {
+		$r = ( new BuildData() )
+			->setMod( $this->getCon()->getModule_IPs() )
+			->build( true );
+		var_dump( $r );
+	}
+
+	private function getIpRefs() {
+		$ipRefs = $this->getCon()
+					   ->getModule_Data()
+					   ->getDbH_ReqLogs()
+					   ->getQuerySelector()
+					   ->getDistinctForColumn( 'ip_ref' );
+		var_dump( $ipRefs );
 	}
 }
--- src/lib/src/Modules/Insights/ActionRouter/Actions/Render/PluginAdminPages/PageDebug.php
+++ src/lib/src/Modules/Insights/ActionRouter/Actions/Render/PluginAdminPages/PageDebug.php
@@ -6,6 +6,7 @@ use FernleafSystems\Wordpress\Plugin\Shield\Modules\Insights\ActionRouter\Action
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Insights\ActionRouter\Actions\Render\Components\Debug\DebugRecentEvents;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Insights\ActionRouter\Constants;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\Plugin\Lib\Debug\Collate;
+use FernleafSystems\Wordpress\Services\Services;
 use FernleafSystems\Wordpress\Services\Utilities\URL;
 
 class PageDebug extends BasePluginAdminPage {
@@ -27,9 +28,31 @@ class PageDebug extends BasePluginAdminPage {
 			$debugExec = '';
 		}
 
+		$availableTests = [];
+		if ( $con->this_req->is_security_admin && Services::Request()->query( 'show' ) ) {
+			$availableTests = array_map(
+				function ( $method ) {
+					return sprintf(
+						'<a href="%s" target="_blank">%s</a>',
+						$this->getCon()
+							 ->getModule_Insights()
+							 ->getUrl_SubInsightsPage( 'debug', $method->getName() ),
+						str_replace( 'dbg_', '', $method->getName() )
+					);
+				},
+				array_filter(
+					( new \ReflectionClass( SimplePluginTests::class ) )->getMethods(),
+					function ( $method ) {
+						return strpos( $method->getName(), 'dbg_' ) === 0;
+					}
+				)
+			);
+		}
+
 		return [
 			'flags'   => [
 				'has_debug_exec' => !empty( $debugExec ),
+				'display_tests'  => !empty( $availableTests ),
 			],
 			'hrefs'   => [
 				'check_visitor_ip_source' => URL::Build( '', [ 'shield_check_ip_source' => '1' ] ),
@@ -38,10 +61,12 @@ class PageDebug extends BasePluginAdminPage {
 				'page_title' => sprintf( __( '%s Debug Page' ), $con->getHumanName() )
 			],
 			'vars'    => [
-				'debug_data' => ( new Collate() )
-					->setMod( $this->getMod() )
-					->run(),
-				'debug_exec' => $debugExec,
+				'debug_data'      => empty( $debugExec ) ?
+					( new Collate() )
+						->setMod( $this->getMod() )
+						->run() : '',
+				'debug_exec'      => $debugExec,
+				'available_tests' => $availableTests,
 			],
 			'content' => [
 				'recent_events' => $con->getModule_Insights()
