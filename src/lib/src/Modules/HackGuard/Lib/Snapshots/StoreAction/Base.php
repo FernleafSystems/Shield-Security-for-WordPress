a0c8d58cdf16bcec20f6ff9552a9cd86649e8849
Paul G <paul@icontrolwp.com>
2023-02-24 12:35:03 +0000

    misc fixes and tweaks

--- src/lib/src/Modules/HackGuard/Lib/Snapshots/StoreAction/Base.php
+++ src/lib/src/Modules/HackGuard/Lib/Snapshots/StoreAction/Base.php
@@ -5,7 +5,6 @@ namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\Lib\Snapshot
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\Lib\Snapshots\StorageDir;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\ModCon;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\ModConsumer;
-use FernleafSystems\Wordpress\Services\Services;
 
 class Base {
 
@@ -13,6 +12,9 @@ class Base {
 
 	public const MOD = ModCon::SLUG;
 
+	/**
+	 * @deprecated 17.0
+	 */
 	private static $tempDir;
 
 	private static $storageDir;
@@ -22,41 +24,6 @@ class Base {
 	}
 
 	protected function getTempDir() :string {
-		{ // TODO: remove this
-			if ( !empty( self::$tempDir ) ) {
-				return self::$tempDir;
-			}
-		}
-
-		if ( empty( self::$storageDir ) ) {
-			self::$storageDir = ( new StorageDir() )->setCon( $this->getCon() );
-		}
-		return self::$storageDir->getTempDir();
-	}
-
-	/**
-	 * @deprecated 16.1.14
-	 */
-	private function generateNewDirName() :string {
-		return $this->getCon()->cache_dir_handler->buildSubDir( 'ptguard-'.wp_generate_password( 16, false ) );
-	}
-
-	/**
-	 * @throws \Exception
-	 * @deprecated 16.1.14
-	 */
-	private function findTempDir() :string {
-		$FS = Services::WpFs();
-		$dir = null;
-		foreach ( $FS->getAllFilesInDir( $this->getCon()->cache_dir_handler->dir() ) as $fileItem ) {
-			if ( strpos( basename( $fileItem ), 'ptguard' ) === 0 && $FS->isDir( $fileItem ) ) {
-				$dir = $fileItem;
-				break;
-			}
-		}
-		if ( empty( $dir ) ) {
-			throw new \Exception( "Dir doesn't exist" );
-		}
-		return $dir;
+		return ( self::$storageDir ?? self::$storageDir = ( new StorageDir() )->setCon( $this->getCon() ) )->getTempDir();
 	}
 }
