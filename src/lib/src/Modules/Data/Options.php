943bc200499a5d7a5d39512ca1d72cf65431ff93
Paulie G <paul@icontrolwp.com>
2023-03-01 16:02:23 +0000

    add modconsumers for more modules

---
+++ src/lib/src/Modules/Data/Options.php
@@ -0,0 +1,7 @@
+<?php declare( strict_types=1 );
+
+namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Data;
+
+class Options extends \FernleafSystems\Wordpress\Plugin\Shield\Modules\BaseShield\Options {
+
+}
---
+++ src/lib/src/Modules/UserManagement/ModConsumer.php
@@ -0,0 +1,24 @@
+<?php declare( strict_types=1 );
+
+namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\UserManagement;
+
+trait ModConsumer {
+
+	use \FernleafSystems\Wordpress\Plugin\Shield\Modules\ModConsumer;
+
+	public function mod() :ModCon {
+		return $this->con()->modules[ ModCon::SLUG ];
+	}
+
+	public function opts() :Options {
+		return $this->mod()->getOptions();
+	}
+
+	/**
+	 * @return ModCon
+	 * @deprecated 17.1
+	 */
+	public function getMod() {
+		return $this->mod();
+	}
+}
