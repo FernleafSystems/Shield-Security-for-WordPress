949396c9577235d44b10d5c244f019c9e22ae16f
Paul G <paul@icontrolwp.com>
2023-09-11 13:47:54 +0100

    provide placeholders to reduce chances of fatal errors during upgrade

---
+++ src/lib/src/ActionRouter/Actions/Render/Components/Docs/DocsChangeLog.php
@@ -0,0 +1,10 @@
+<?php declare( strict_types=1 );
+
+namespace FernleafSystems\Wordpress\Plugin\Shield\ActionRouter\Actions\Render\Components\Docs;
+
+/**
+ * @deprecated 18.3.0
+ */
+class DocsChangeLog extends \FernleafSystems\Wordpress\Plugin\Shield\ActionRouter\Actions\Render\BaseRender {
+
+}
