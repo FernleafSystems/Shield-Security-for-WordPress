057f9a89c3be6e90225d6bd27108e400f8cbfd2c
Paul G <paul@icontrolwp.com>
2023-11-20 22:22:16 +0000

    add CSS to hide notifications of other plugins try to prevent repeated call to 'what is my ip' update version for release - ensure the IP Autodetect javascript is silent when there's an error - ensure IP detect will only run in the Shield admin for now

--- assets/js/components/ips/IpSourceAutoDetect.js
+++ assets/js/components/ips/IpSourceAutoDetect.js
@@ -1,34 +1,34 @@
-import $ from 'jquery';
 import { BaseAutoExecComponent } from "../BaseAutoExecComponent";
 import { AjaxService } from "../services/AjaxService";
 
 export class IpSourceAutoDetect extends BaseAutoExecComponent {
 
 	canRun() {
-		return this._base_data.flags.is_check_required;
+		return this._base_data.flags.is_check_required
+			&& typeof fetch !== typeof undefined;
 	}
 
 	run() {
-		$.getJSON( this._base_data.url, ( resp ) => {
+		fetch( this._base_data.url )
+		.then( raw => raw.json() )
+		.then( resp => {
 			if ( typeof resp !== 'undefined' && typeof resp[ 'ip' ] !== 'undefined' ) {
 
-				let self = this;
 				this._base_data.ajax[ 'ip' ] = resp[ 'ip' ];
 
 				( new AjaxService() )
-				.send( self._base_data.ajax )
+				.bg( this._base_data.ajax )
 				.then( ( resp ) => {
-					if ( resp.success ) {
-						let msg = self._base_data.strings.source_found
-							+ ' ' + self._base_data.strings.ip_source
-							+ ': ' + resp.data.ip_source;
-						if ( !self._base_data.flags.quiet ) {
-							alert( msg );
-						}
+					if ( resp.success && !this._base_data.flags.quiet ) {
+						alert(
+							this._base_data.strings.source_found
+							+ ' ' + this._base_data.strings.ip_source
+							+ ': ' + resp.data.ip_source
+						);
 					}
 				} )
 				.finally();
 			}
-		} );
+		} )
 	};
 }
