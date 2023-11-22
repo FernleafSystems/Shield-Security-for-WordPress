8e86847172010141e3518c6823e6aa19808177f7
Paulie G <paul@icontrolwp.com>
2023-11-22 15:06:09 +0000

    refactor wpadmin JS App

--- assets/plugin-wpadmin.js
+++ assets/plugin-wpadmin.js
@@ -1,27 +1,4 @@
 import "./css/plugin-wpadmin.scss";
+import { AppWpAdmin } from "./js/app/AppWpAdmin";
 
-import { DashboardWidget } from "./js/components/general/DashboardWidget";
-import { HackGuardPluginReinstall } from "./js/components/general/HackGuardPluginReinstall";
-import { IpSourceAutoDetect } from "./js/components/ips/IpSourceAutoDetect";
-import { NoticeHandler } from "./js/components/notices/NoticeHandler";
-import { SecurityAdmin } from "./js/components/general/SecurityAdmin";
-import { ShieldServicesWpAdmin } from "./js/services/ShieldServicesWpAdmin";
-import { ShieldEventsHandler } from "./js/services/ShieldEventsHandler";
-
-window.addEventListener( 'load', () => {
-	global.shieldServices = ShieldServicesWpAdmin.Instance();
-
-	if ( 'shield_vars_wpadmin' in window ) {
-
-		global.shieldEventsHandler_Main = new ShieldEventsHandler( {
-			events_container_selector: 'body'
-		} );
-
-		const comps = window.shield_vars_wpadmin.comps;
-		( 'dashboard_widget' in comps ) && new DashboardWidget( comps.dashboard_widget );
-		( 'ip_detect' in comps ) && new IpSourceAutoDetect( comps.ip_detect );
-		( 'notices' in comps ) && new NoticeHandler( comps.notices );
-		( 'plugin_reinstall' in comps ) && new HackGuardPluginReinstall( comps.plugin_reinstall );
-		( 'sec_admin' in comps ) && new SecurityAdmin( comps.sec_admin );
-	}
-}, false );
+global.shieldAppMain = new AppWpAdmin();
