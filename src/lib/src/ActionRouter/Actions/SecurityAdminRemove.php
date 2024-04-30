f21552f005e9b48b338b29fa3ee531b089f9b3c0
Paulie G <paul@icontrolwp.com>
2024-04-30 16:11:46 +0100

    prevent removal of Security Admin PIN through CSRF

--- src/lib/src/ActionRouter/Actions/SecurityAdminRemove.php
+++ src/lib/src/ActionRouter/Actions/SecurityAdminRemove.php
@@ -2,15 +2,18 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\ActionRouter\Actions;
 
+use FernleafSystems\Wordpress\Plugin\Shield\ActionRouter\Actions\Traits\NonceVerifyRequired;
 use FernleafSystems\Wordpress\Plugin\Shield\Enum\EnumModules;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\SecurityAdmin\Lib\SecurityAdmin\Ops\RemoveSecAdmin;
 
 class SecurityAdminRemove extends SecurityAdminBase {
 
+	use NonceVerifyRequired;
+
 	public const SLUG = 'secadmin_remove_confirm';
 
 	protected function exec() {
-		( new RemoveSecAdmin() )->remove( (bool)$this->action_data[ 'quietly' ] ?? false );
+		( new RemoveSecAdmin() )->remove( (bool)( $this->action_data[ 'quietly' ] ?? false ) );
 		$this->response()->next_step = [
 			'type' => 'redirect',
 			'url'  => self::con()->plugin_urls->modCfg( EnumModules::SECURITY_ADMIN ),
