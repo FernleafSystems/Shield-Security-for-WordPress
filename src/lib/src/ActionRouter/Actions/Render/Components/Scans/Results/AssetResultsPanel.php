33889522c32a6afb7750cd710e6c43025af8364a
Paulie G <paul@icontrolwp.com>
2024-05-20 10:41:56 +0100

    further refinement of display of plugin and theme scan results panels

--- src/lib/src/ActionRouter/Actions/Render/Components/Scans/Results/AssetResultsPanel.php
+++ src/lib/src/ActionRouter/Actions/Render/Components/Scans/Results/AssetResultsPanel.php
@@ -11,6 +11,7 @@ class AssetResultsPanel extends PluginThemesBase {
 	public const TEMPLATE = '/wpadmin_pages/insights/scans/results/section/assets/%s_panel.twig';
 
 	protected function getRenderData() :array {
+		$con = self::con();
 		$type = $this->action_data[ 'asset_type' ];
 		$uniqID = $this->action_data[ 'unique_id' ];
 
@@ -23,16 +24,27 @@ class AssetResultsPanel extends PluginThemesBase {
 
 		return Services::DataManipulation()->mergeArraysRecursive( parent::getRenderData(), [
 			'strings' => [
-				'no_files'      => __( "Scans didn't detect any modified or unrecognised files.", 'wp-simple-firewall' ),
-				'files_found'   => __( "Scans detected 1 or more modified or unrecognised files.", 'wp-simple-firewall' ),
-				'not_active'    => __( "This isn't active and should be uninstalled.", 'wp-simple-firewall' ),
-				'wporg_ok'      => __( "This is installed from WordPress.org so actions such as file repair and file diff are available.", 'wp-simple-firewall' ),
-				'not_wporg'     => __( "This isn't installed from WordPress.org so actions such as file repair and file diff aren't available.", 'wp-simple-firewall' ),
-				'no_tags'       => __( "The developer chose not to use SVN tags for this version, so actions such as file repair and file diff aren't available.", 'wp-simple-firewall' ),
-				'go_to_plugins' => sprintf( __( 'Go To %s', 'wp-simple-firewall' ), __( 'Plugins' ) ),
+				'no_files'         => __( "Scans didn't detect any modified or unrecognised files", 'wp-simple-firewall' ),
+				'files_found'      => __( "Scans detected modified/unrecognised files", 'wp-simple-firewall' ),
+				'not_active'       => __( 'Inactive' ),
+				'should_uninstall' => __( "Should be uninstalled", 'wp-simple-firewall' ),
+				'wporg_ok'         => __( "Auto File Repair and File Diff are available", 'wp-simple-firewall' ),
+				'not_wporg'        => __( "Auto File Repair and File Diff aren't available.", 'wp-simple-firewall' ),
+				'no_tags'          => __( "The developer chose not to use SVN tags for this version, so actions such as file repair and file diff aren't available.", 'wp-simple-firewall' ),
+				'manage'           => __( 'Manage', 'wp-simple-firewall' ),
+				'ignore'           => __( 'Ignore', 'wp-simple-firewall' ),
+				'updates'          => __( 'Updates', 'wp-simple-firewall' ),
 			],
 			'hrefs'   => [
-				'page_plugins' => Services::WpGeneral()->getAdminUrl_Plugins()
+				'page_manage' => $type === 'plugin' ?
+					Services::WpGeneral()->getAdminUrl_Plugins() : Services::WpGeneral()->getAdminUrl_Themes(),
+			],
+			'imgs'    => [
+				'svgs' => [
+					'shield_x'           => $con->svgs->raw( 'shield-fill-x.svg' ),
+					'shield_check'       => $con->svgs->raw( 'shield-fill-check.svg' ),
+					'shield_exclamation' => $con->svgs->raw( 'shield-fill-exclamation.svg' ),
+				],
 			],
 			'vars'    => [
 				'asset_data' => $data,
