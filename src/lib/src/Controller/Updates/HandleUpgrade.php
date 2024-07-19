540b8c169db18817475140db6bd750c0af2a292f
Paulie G <paul@icontrolwp.com>
2024-07-19 11:21:18 +0100

    add auto cache clear on upgrade

--- src/lib/src/Controller/Updates/HandleUpgrade.php
+++ src/lib/src/Controller/Updates/HandleUpgrade.php
@@ -32,6 +32,7 @@ class HandleUpgrade {
 
 			Services::ServiceProviders()->clearProviders();
 			$con->plugin->deleteAllPluginCrons();
+			$this->clearCaches();
 
 			if ( $con->extensions_controller->canRunExtensions() ) {
 				foreach ( $con->extensions_controller->getAvailableExtensions() as $availableExtension ) {
@@ -44,6 +45,33 @@ class HandleUpgrade {
 		} );
 
 		$con->cfg->previous_version = $con->cfg->version();
+	}
+
+	public function clearCaches() {
+		\array_map(
+			function ( $func ) {
+				\function_exists( $func ) && call_user_func( $func );
+			},
+			[
+				'wpfc_clear_all_cache', // WP Fastest Cache
+				'rocket_clean_domain', // WP Rocket
+				'w3tc_pgcache_flush', // W3 Total Cache
+			]
+		);
+		if ( \function_exists( 'wp_cache_clean_cache' ) ) {
+			// WP Super Cache
+			global $file_prefix;
+			wp_cache_clean_cache( $file_prefix, true );
+		}
+		if ( \class_exists( '\LiteSpeed\Purge' ) && \method_exists( '\LiteSpeed\Purge', 'purge_all' ) ) {
+			\LiteSpeed\Purge::purge_all();
+		}
+		if ( \class_exists( '\WP_Optimize' ) && \method_exists( '\WP_Optimize', 'get_page_cache' ) ) {
+			$wpOptimisePageCache = \WP_Optimize()->get_page_cache();
+			if ( \method_exists( $wpOptimisePageCache, 'purge' ) ) {
+				$wpOptimisePageCache->purge();
+			}
+		}
 	}
 
 	protected function upgradeModule() {
