2a91a79e32f31bec85e53a32e5e6d9a3dfd2ceb2
Paulie G <paul@icontrolwp.com>
2024-07-25 13:12:51 +0100

    attempt to offset fatal error during upgrade

--- src/lib/src/Rules/Build/Core/BotTrack404.php
+++ src/lib/src/Rules/Build/Core/BotTrack404.php
@@ -22,24 +22,29 @@ class BotTrack404 extends BuildRuleIpsBase {
 
 	protected function getConditions() :array {
 		$whitelistedConditions = [];
-		$whitelistedConditions[] = [
-			'conditions' => Conditions\MatchRequestPath::class,
-			'logic'      => Enum\EnumLogic::LOGIC_INVERT,
-			'params'     => [
-				'match_type' => Enum\EnumMatchTypes::MATCH_TYPE_REGEX,
-				'match_path' => sprintf( "#\\.(%s)$#i", \implode( '|', self::con()->comps->bot_signals->getAllowableExt404s() ) ),
-			],
-		];
-		foreach ( self::con()->comps->bot_signals->getAllowablePaths404s() as $allowablePaths404 ) {
+		if ( \method_exists( self::con()->comps->bot_signals, 'getAllowableExt404s' ) ) {
 			$whitelistedConditions[] = [
 				'conditions' => Conditions\MatchRequestPath::class,
 				'logic'      => Enum\EnumLogic::LOGIC_INVERT,
 				'params'     => [
 					'match_type' => Enum\EnumMatchTypes::MATCH_TYPE_REGEX,
-					'match_path' => $allowablePaths404,
+					'match_path' => sprintf( "#\\.(%s)$#i", \implode( '|', self::con()->comps->bot_signals->getAllowableExt404s() ) ),
 				],
 			];
 		}
+		if ( \method_exists( self::con()->comps->bot_signals, 'getAllowablePaths404s' ) ) {
+			foreach ( self::con()->comps->bot_signals->getAllowablePaths404s() as $allowablePaths404 ) {
+				$whitelistedConditions[] = [
+					'conditions' => Conditions\MatchRequestPath::class,
+					'logic'      => Enum\EnumLogic::LOGIC_INVERT,
+					'params'     => [
+						'match_type' => Enum\EnumMatchTypes::MATCH_TYPE_REGEX,
+						'match_path' => $allowablePaths404,
+					],
+				];
+			}
+		}
+
 		$whitelistedConditions[] = [
 			'conditions' => Conditions\IsRequestToInvalidPlugin::class,
 		];
