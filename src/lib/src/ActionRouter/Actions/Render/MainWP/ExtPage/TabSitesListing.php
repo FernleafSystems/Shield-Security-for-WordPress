8aba0c1b01dff095b03b218c42e6ba60d379a30f
Paulie G <paul@icontrolwp.com>
2024-08-12 12:55:34 +0100

    fixes for MainWP integration - ensure we correctly pull the Meter Components Grade - actions menu is built more consistently - can now switch-on mainwp option for individual sites

--- src/lib/src/ActionRouter/Actions/Render/MainWP/ExtPage/TabSitesListing.php
+++ src/lib/src/ActionRouter/Actions/Render/MainWP/ExtPage/TabSitesListing.php
@@ -2,6 +2,11 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\ActionRouter\Actions\Render\MainWP\ExtPage;
 
+use FernleafSystems\Wordpress\Plugin\Shield\Controller\Plugin\PluginNavs;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\Integrations\Lib\MainWP\Server\Data\ClientPluginStatus;
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\Integrations\Lib\MainWP\Server\Data\LoadShieldSyncData;
+use FernleafSystems\Wordpress\Services\Services;
+
 class TabSitesListing extends BaseSubPage {
 
 	public const SLUG = 'mainwp_page_sites_listing';
@@ -52,5 +57,119 @@ class TabSitesListing extends BaseSubPage {
 				'stats_head' => $statsHead,
 			],
 		];
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	protected function buildEntireSiteData( array $site ) :array {
+		$mwpSite = $this->getSiteByID( (int)$site[ 'id' ] );
+		$sync = LoadShieldSyncData::Load( $mwpSite );
+
+		$status = ( new ClientPluginStatus() )
+			->setMwpSite( $mwpSite )
+			->detect();
+
+		$shd = $sync->getRawData();
+
+		$shd[ 'status_key' ] = \key( $status );
+		$shd[ 'status' ] = \current( $status );
+
+		$shd[ 'is_active' ] = $shd[ 'status_key' ] === ClientPluginStatus::ACTIVE;
+		$shd[ 'is_inactive' ] = $shd[ 'status_key' ] === ClientPluginStatus::INACTIVE;
+		$shd[ 'is_notinstalled' ] = $shd[ 'status_key' ] === ClientPluginStatus::NOT_INSTALLED;
+		$shd[ 'is_notpro' ] = $shd[ 'status_key' ] === ClientPluginStatus::NOT_PRO;
+		$shd[ 'is_mwpnoton' ] = $shd[ 'status_key' ] === ClientPluginStatus::MWP_NOT_ON;
+		$shd[ 'is_sync_rqd' ] = $shd[ 'status_key' ] === ClientPluginStatus::NEED_SYNC;
+		$shd[ 'is_client_older' ] = $shd[ 'status_key' ] === ClientPluginStatus::VERSION_OLDER_THAN_SERVER;
+		$shd[ 'is_client_newer' ] = $shd[ 'status_key' ] === ClientPluginStatus::VERSION_NEWER_THAN_SERVER;
+		$shd[ 'is_version_mismatch' ] = \in_array( $shd[ 'status_key' ], [
+			ClientPluginStatus::VERSION_NEWER_THAN_SERVER,
+			ClientPluginStatus::VERSION_OLDER_THAN_SERVER,
+		] );
+		$shd[ 'can_sync' ] = \in_array( $shd[ 'status_key' ], [
+			ClientPluginStatus::ACTIVE,
+			ClientPluginStatus::NEED_SYNC,
+			ClientPluginStatus::VERSION_NEWER_THAN_SERVER,
+			ClientPluginStatus::VERSION_OLDER_THAN_SERVER,
+		] );
+		$shd[ 'has_update' ] = (bool)$sync->meta->has_update;
+		$shd[ 'has_issues' ] = false;
+
+		if ( $shd[ 'is_active' ] ) {
+
+			$shd[ 'sync_at_text' ] = Services::WpGeneral()->getTimeStringForDisplay( $sync->meta->sync_at );
+			$shd[ 'sync_at_diff' ] = Services::Request()
+											 ->carbon()
+											 ->setTimestamp( $sync->meta->sync_at )
+											 ->diffForHumans();
+
+			$totalIssues = \array_sum( $sync->scan_issues ?? [] );
+			if ( $totalIssues === 0 ) {
+				$shd[ 'issues' ] = __( 'No Issues', 'wp-simple-firewall' );
+				$shd[ 'has_issues' ] = false;
+			}
+			else {
+				$shd[ 'has_issues' ] = true;
+				$shd[ 'issues' ] = $totalIssues;
+			}
+
+			$shd[ 'href_issues' ] = $this->getJumpUrlFor(
+				(string)$site[ 'id' ],
+				self::con()->plugin_urls->adminTopNav( PluginNavs::NAV_SCANS, PluginNavs::SUBNAV_SCANS_RESULTS )
+			);
+			$gradeLetter = $sync->integrity[ 'totals' ][ 'letter_score' ] ?? '-';
+			$shd[ 'grades' ] = [
+				'href'      => $this->getJumpUrlFor( (string)$site[ 'id' ], self::con()->plugin_urls->adminHome() ),
+				'integrity' => $gradeLetter,
+				'good'      => \in_array( $gradeLetter, [ 'A', 'B' ] ),
+			];
+
+			$shd[ 'href_manage' ] = $this->createInternalExtensionHref( [
+				'tab'     => TabSiteManage::TAB,
+				'site_id' => $site[ 'id' ],
+			] );
+		}
+
+		$shd[ 'site_actions' ] = $this->determineSiteActions( $shd );
+
+		$site[ 'shield' ] = $shd;
+		$site[ 'hrefs' ] = [
+			'manage_site' => $this->createInternalExtensionHref( [
+				'tab'     => 'manage_site',
+				'site_id' => $site[ 'id' ],
+			] )
+		];
+
+		return $site;
+	}
+
+	private function determineSiteActions( array $flags ) :array {
+		$actions = [];
+		if ( $flags[ 'can_sync' ] ) {
+			$actions[] = 'sync';
+		}
+
+		if ( $flags[ 'has_update' ] ) {
+			$actions[] = 'update';
+		}
+
+		if ( $flags[ 'is_notinstalled' ] ) {
+			$actions[] = 'install';
+		}
+		elseif ( $flags[ 'is_inactive' ] ) {
+			$actions[] = 'activate';
+		}
+		else {
+			$actions[] = 'deactivate';
+		}
+
+		$actions[] = 'license';
+
+		if ( $flags[ 'is_mwpnoton' ] ) {
+			$actions[] = 'mwp_on';
+		}
+
+		return $actions;
 	}
 }
--- src/lib/src/Modules/Integrations/Lib/MainWP/Client/Actions/Sync.php
+++ src/lib/src/Modules/Integrations/Lib/MainWP/Client/Actions/Sync.php
@@ -3,10 +3,7 @@
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Integrations\Lib\MainWP\Client\Actions;
 
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\HackGuard\Scan\Results\Counts;
-use FernleafSystems\Wordpress\Plugin\Shield\Modules\Plugin\Lib\{
-	ImportExport,
-	MeterAnalysis
-};
+use FernleafSystems\Wordpress\Plugin\Shield\Modules\Plugin\Lib\MeterAnalysis;
 use FernleafSystems\Wordpress\Plugin\Shield\Modules\PluginControllerConsumer;
 use FernleafSystems\Wordpress\Services\Services;
 
@@ -20,13 +17,8 @@ class Sync {
 
 	public function run() :array {
 		$con = self::con();
-		$additional = $this->isPermitted() ? [
-			'options'     => ( new ImportExport\Export() )->getFullTransferableOptionsExport(),
-			'integrity'   => ( new MeterAnalysis\Handler() )->getMeter( MeterAnalysis\Meter\MeterSummary::class ),
-			'scan_issues' => ( new Counts() )->all(),
-		] : [];
-		return \array_merge( [
-			'meta' => [
+		return [
+			'meta'        => [
 				'is_pro'       => $con->isPremiumActive(),
 				'is_mainwp_on' => $this->isPermitted(),
 				'installed_at' => $con->comps->opts_lookup->getInstalledAt(),
@@ -34,6 +26,12 @@ class Sync {
 				'version'      => $con->cfg->version(),
 				'has_update'   => Services::WpPlugins()->isUpdateAvailable( $con->base_file ),
 			],
-		], $additional );
+			'options'     => [], // not necessary yet ( new ImportExport\Export() )->getFullTransferableOptionsExport(),
+			'integrity'   => \array_intersect_key(
+				( new MeterAnalysis\Handler() )->getMeter( MeterAnalysis\Meter\MeterSummary::class ),
+				\array_flip( [ 'status', 'totals' ] )
+			),
+			'scan_issues' => ( new Counts() )->all(),
+		];
 	}
 }
