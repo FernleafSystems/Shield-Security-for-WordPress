ff8831a93fbe589faa7645f60070cb73517ba103
Paulie G <paul@icontrolwp.com>
2024-12-10 09:23:55 +0000

    fix for ninja forms firing their filter before we're setup

--- src/lib/src/Modules/Integrations/Lib/Bots/Spam/Handlers/NinjaForms.php
+++ src/lib/src/Modules/Integrations/Lib/Bots/Spam/Handlers/NinjaForms.php
@@ -17,20 +17,51 @@ class NinjaForms extends Base {
 
 	protected function run() {
 
-		add_filter( 'ninja_forms_register_actions', function ( $actions ) {
-			$actions[ 'shieldantibot' ] = ( new NinjaForms_ShieldSpamAction() )->setHandler( $this );
-			return $actions;
-		}, 1000 );
+		/**
+		 * Ninja forms fires the filter very early in the load, which is cracker stuff, so we must manually add our
+		 * action to the list, taking care that public access to the `actions` array is maintained going forward.
+		 */
+		if ( did_action( 'plugins_loaded' ) && $this->canAddDirectlyToActionsProperty() ) {
+			\Ninja_Forms::instance()->actions = $this->registerShieldAction( \Ninja_Forms::instance()->actions );
+		}
+		else {
+			add_filter( 'ninja_forms_register_actions', function ( $actions ) {
+				return \is_array( $actions ) ? $this->registerShieldAction( $actions ) : $actions;
+			}, 1000 );
+		}
 
 		add_filter( 'ninja_forms_submission_actions', function ( $actions ) {
-			$actions[] = [
-				'id'       => 'shieldantibot',
-				'settings' => [
-					'active' => true,
-					'type'   => 'shieldantibot',
-				]
-			];
+			if ( \is_array( $actions ) ) {
+				$actions[] = [
+					'id'       => 'shieldantibot',
+					'settings' => [
+						'active' => true,
+						'type'   => 'shieldantibot',
+					]
+				];
+			}
 			return $actions;
 		}, 1000 );
+	}
+
+	private function registerShieldAction( array $actions ) :array {
+		$actions[ 'shieldantibot' ] = ( new NinjaForms_ShieldSpamAction() )->setHandler( $this );
+		return $actions;
+	}
+
+	/**
+	 * Checks that the method is there and that there is a properties named "actions" on that class, and that it's
+	 * public
+	 */
+	private function canAddDirectlyToActionsProperty() :bool {
+		$can = false;
+		if ( \method_exists( \Ninja_Forms::class, 'instance' ) ) {
+			try {
+				$can = ( new \ReflectionClass( \Ninja_Forms::class ) )->getProperty( 'actions' )->isPublic();
+			}
+			catch ( \Exception $e ) {
+			}
+		}
+		return $can;
 	}
 }
