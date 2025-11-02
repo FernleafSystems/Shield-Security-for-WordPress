a15e2a2047e7bdff246b5f6702c47103c001fea4
Richard Wawrzyniak <richard@w8k.dev>
2025-11-02 13:07:40 +0000

    Add entry spam note

--- src/lib/src/Modules/Integrations/Lib/Bots/Spam/Handlers/GravityForms.php
+++ src/lib/src/Modules/Integrations/Lib/Bots/Spam/Handlers/GravityForms.php
@@ -2,12 +2,23 @@
 
 namespace FernleafSystems\Wordpress\Plugin\Shield\Modules\Integrations\Lib\Bots\Spam\Handlers;
 
+use GFCommon;
+
 class GravityForms extends Base {
 
 	protected function run() {
-		add_filter( 'gform_entry_is_spam', function ( $wasSpam ) {
-			return $wasSpam || $this->isBotBlockRequired();
-		}, 1000 );
+		add_filter( 'gform_entry_is_spam', function ( $wasSpam, $form ) {
+			if ( $wasSpam ) {
+				return true;
+			}
+
+			$is_spam = $this->isBotBlockRequired();
+			if ( $is_spam && method_exists( 'GFCommon', 'set_spam_filter' ) ) {
+				GFCommon::set_spam_filter( $form['id'], 'Shield Security', $this->getCommonSpamMessage() );
+			}
+
+			return $is_spam;
+		}, 1000, 2 );
 	}
 
 	protected static function ProviderMeetsRequirements() :bool {
